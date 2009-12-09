cb7d5480944229e1a5ddfa9d1113903628765584
Wynn Netherland <wynn@squeejee.com>
2009-12-08 20:52:22 -0600

    Initial commit to octopussy.

---
+++ test/test_octopussy.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestOctopussy < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
