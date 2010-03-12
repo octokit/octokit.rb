a1695a636a6e68d3bb64ea21b5fe6fa08dade7b5
Rein Henrichs <reinh@reinh.com>
2010-03-11 20:54:17 -0800

    Use SingleForwardable for Octopussy delegation

--- lib/octopussy.rb
+++ lib/octopussy.rb
@@ -1,3 +1,5 @@
+require 'forwardable'
+
 require 'httparty'
 require 'hashie'
 Hash.send :include, Hashie::HashExtensions
@@ -11,6 +13,8 @@ require 'octopussy/client'
 
 
 module Octopussy
+  extend SingleForwardable
+
   class OctopussyError < StandardError
     attr_reader :data
 
@@ -26,105 +30,23 @@ module Octopussy
 
   class Unavailable   < StandardError; end
   class InformOctopussy < StandardError; end
-  class NotFound      < StandardError; end
-  
-  
-  def self.search_users(q)
-    Client.new.search_users(q)
-  end
-  
-  def self.user(login)
-    Client.new.user(login)
-  end
-  
-  def self.followers(login)
-    Client.new.followers(login)
-  end
-  
-  def self.following(login)
-    Client.new.following(login)
-  end
-  
-  def self.follows?(username, target)
-    Client.new.follows?(username, target)
-  end
-  
-  def self.watched(login)
-    Client.new.watched(login)
-  end
-  
+  class NotFound        < StandardError; end
+
+  def self.client; Client.new end
+
+  # Users
+  def_delegators :client, :search_users, :user, :followers, :following, :follows?, :watched
+
   # Issues
-  
-  def self.search_issues(repo, state, q)
-    Client.new.search_issues(repo, state, q)
-  end
-  
-  # repo, state
-  def self.issues(repo, state)
-    Client.new.issues(repo, state)
-  end
-  
-  # repo, id
-  def self.issue(repo, id)
-    Client.new.issue(repo, id)
-  end
-  
+  def_delegators :client, :search_issues, :issues, :issue
+
   # Repos
-  
-  def self.search_repos(q)
-    Client.new.search_repos(q)
-  end
-  
-  def self.repo(repo)
-    Client.new.repo(repo)
-  end
-  
-  def self.list_repos(username)
-    Client.new.list_repos(username)
-  end
-  
-  def self.collaborators(repo)
-    Client.new.collaborators(repo)
-  end
-  
-  def self.network(repo)
-    Client.new.network(repo)
-  end
-  
-  def self.languages(repo)
-    Client.new.languages(repo)
-  end
-  
-  def self.tags(repo)
-    Client.new.tags(repo)
-  end
-  
-  def self.branches(repo)
-    Client.new.branches(repo)
-  end
-  
+  def_delegators :client, :branches, :collaborators, :languages, :list_repos,
+                 :network, :repo, :search_repos, :tags
+
   # Network Meta
+  def_delegators :client, :network_meta, :network_data
 
-  def self.network_meta(repo)
-    Client.new.network_meta(repo)
-  end
-  
-  def self.network_data(repo, nethash)
-    Client.new.network_data(repo, nethash)
-  end
-  
   # Trees
-
-  def self.tree(repo, sha)
-    Client.new.tree(repo, sha)
-  end
-
-  def self.blob(repo, sha, path)
-    Client.new.blob(repo, sha, path)
-  end
-  
-  def self.raw(repo, sha)
-    Client.new.raw(repo, sha)
-  end
-  
+  def_delegators :client, :tree, :blob, :raw
 end
