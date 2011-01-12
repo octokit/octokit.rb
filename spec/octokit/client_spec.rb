aa5b5a51827b19d03de3dca05ba0e672740176c2
Erik Michaels-Ober <sferik@gmail.com>
2011-01-11 19:32:23 -0800

    Add Client specs

---
+++ spec/octokit/client_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path('../../helper', __FILE__)
+
+describe Octokit::Client do
+  it "should connect using the endpoint configuration" do
+    client = Octokit::Client.new
+    endpoint = URI.parse(client.endpoint)
+    connection = client.send(:connection).build_url(nil).to_s
+    connection.should == endpoint.to_s
+  end
+end
