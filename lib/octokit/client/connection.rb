e3cbaad8a314f3f10e3046c8640634d1e0e61dcb
Wynn Netherland <wynn.netherland@gmail.com>
2011-01-18 21:17:09 -0600

    timeline specs, refactor

--- lib/octokit/client/connection.rb
+++ lib/octokit/client/connection.rb
@@ -7,7 +7,7 @@ module Octokit
     module Connection
       private
 
-      def connection(raw=false)
+      def connection(raw=false, authenticate=true)
         options = {
           :proxy => proxy,
           :ssl => {:verify => false},
@@ -16,7 +16,7 @@ module Octokit
 
         Faraday::Connection.new(options) do |connection|
           connection.adapter(adapter)
-          connection.basic_auth authentication[:login], authentication[:password] if authenticated?
+          connection.basic_auth authentication[:login], authentication[:password] if authenticate and authenticated?
           connection.use Faraday::Response::RaiseError
           unless raw
             case format.to_s.downcase
