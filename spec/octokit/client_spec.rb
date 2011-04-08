64b241fc918be441a9878580e869639e9459eb72
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2011-04-08 08:54:25 +0800

    should support basic auth

--- spec/octokit/client_spec.rb
+++ spec/octokit/client_spec.rb
@@ -8,5 +8,14 @@ describe Octokit::Client do
     connection = client.send(:connection).build_url(nil).to_s
     connection.should == endpoint.to_s
   end
+  
+  it 'should work with basic auth' do
+    stub_request(:get, "https://foo%2Ftoken:bar@github.com/api/v2/json/commits/list/baz/quux/master").
+      with(:headers => {'Accept'=>'*/*'}).
+      to_return(:status => 200, :body => '{"commits":[]}', :headers => {})
+    proc {
+      Octokit::Client.new(:login => 'foo', :token => 'bar').commits('baz/quux')
+    }.should_not raise_exception
+  end
 
 end
