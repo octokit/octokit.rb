f7dd455fe465658f079eeb86a556afce9d6ed5c2
Erik Michaels-Ober <sferik@gmail.com>
2011-04-26 10:14:05 -0700

    Cleanup EOL whitespace

--- spec/octokit/client_spec.rb
+++ spec/octokit/client_spec.rb
@@ -8,7 +8,7 @@ describe Octokit::Client do
     connection = client.send(:connection).build_url(nil).to_s
     connection.should == endpoint.to_s
   end
-  
+
   it 'should work with basic auth' do
     stub_request(:get, "https://foo%2Ftoken:bar@github.com/api/v2/json/commits/list/baz/quux/master").
       with(:headers => {'Accept'=>'*/*'}).
