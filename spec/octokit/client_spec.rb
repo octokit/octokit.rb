1950d944e5d84f077e5234bc6b19da8176e94393
Erik Michaels-Ober <sferik@gmail.com>
2011-04-29 19:33:39 -0700

    Fix specs in Ruby 1.8

--- spec/octokit/client_spec.rb
+++ spec/octokit/client_spec.rb
@@ -19,9 +19,9 @@ describe Octokit::Client do
   end
 
   it 'should work with basic auth and password' do
-    stub_request(:get, "https://foo:bar@github.com/api/v2/json/commits/list/baz/quux/master").
-             with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-             to_return(:status => 200, :body => '{"commits":[]}', :headers => {})
+    stub_request(:get, "https://foo%2Fbar@github.com/api/v2/json/commits/list/baz/quux/master").
+      with(:headers => {'Accept'=>'*/*'}).
+      to_return(:status => 200, :body => '{"commits":[]}', :headers => {})
     proc {
       Octokit::Client.new(:login => 'foo', :password => 'bar').commits('baz/quux')
     }.should_not raise_exception
