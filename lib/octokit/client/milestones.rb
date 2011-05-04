4817176e2c5e9dbbecbb4bcc355420560d84c831
Clint Shryock <clint@ctshryock.com>
2011-05-03 21:37:40 -0500

    Add `create_milestone` to milestones - add test - Documentation for `list_milestones`

--- lib/octokit/client/milestones.rb
+++ lib/octokit/client/milestones.rb
@@ -2,10 +2,26 @@ module Octokit
   class Client
     module Milestones
 
+      # List milestones for a repository
+      #
+      # @param repository [String, Repository, Hash] A GitHub repository.
+      # @param options [Hash] A customizable set of options.
+      # @option options [Integer] :milestone Milestone number.
+      # @option options [String] :state (open) State: <tt>open</tt> or <tt>closed</tt>.
+      # @option options [String] :sort (created) Sort: <tt>created</tt>, <tt>updated</tt>, or <tt>comments</tt>.
+      # @option options [String] :direction (desc) Direction: <tt>asc</tt> or <tt>desc</tt>.
+      # @return [Array] A list of milestones for a repository.
+      # @see http://developer.github.com/v3/issues/milestones/#List-Milestones-for-an-Issue
+      # @example List milestones for a repository
+      #   Octokit.list_milestones("sferik/rails_admin")
       def list_milestones(repository, options={})
         get("/repos/#{Repository.new(repository)}/milestones", options, 3)
       end
-      alias :milstones :list_milestones
+      alias :milestones :list_milestones
+
+      def create_milestone(repository, title, options={})
+        post("/repos/#{Repository.new(repository)}/milestones", options.merge({:title => title}), 3)
+      end
 
     end
   end
--- spec/octokit/client/milestones_spec.rb
+++ spec/octokit/client/milestones_spec.rb
@@ -9,12 +9,24 @@ describe Octokit::Client::Milestones do
 
   describe ".list_milestones" do
 
-    it "should list milestones belonging to an issue" do
+    it "should list milestones belonging to repository" do
       stub_request(:get, "https://api.github.com/repos/pengwynn/octokit/milestones").
-        to_return(:status => 200, :body => fixture('milestones.json'))
+        to_return(:status => 200, :body => fixture('v3/milestones.json'))
       milestones = @client.list_milestones("pengwynn/octokit")
       milestones.first.description.should == "Add support for API v3"
     end
 
   end
+
+  describe ".create_milestone" do
+
+    it "should create a new milestone belonging to a repository" do
+      stub_request(:post, "https://api.github.com/repos/pengwynn/octokit/milestones").
+        to_return(:status => 201, :body => fixture('v3/milestone.json'))
+      milestone = @client.create_milestone("pengwynn/octokit", :title => 'demo')
+      milestone.title.should == "demo"
+    end
+
+  end
+
 end
