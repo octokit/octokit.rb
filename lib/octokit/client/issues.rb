6ec62847d9fb576c8ac9102c516c1a7e473f96a9
Clint Shryock <clint@ctshryock.com>
2011-05-14 14:10:31 -0500

    Added more documentation for the Issues class

--- lib/octokit/client/issues.rb
+++ lib/octokit/client/issues.rb
@@ -43,7 +43,7 @@ module Octokit
       # @param body [String] A concise description
       # @return [Issue] Your newly created issue
       # @see http://develop.github.com/p/issues.html
-      # @example List issues for a repository
+      # @example Create a new Issues for a repository
       #   Octokit.create_issue("sferik/rails_admin")
       def create_issue(repo, title, body, options={})
         post("api/v2/json/issues/open/#{Repository.new(repo)}", options.merge({:title => title, :body => body}))['issue']
@@ -62,18 +62,58 @@ module Octokit
         get("api/v2/json/issues/show/#{Repository.new(repo)}/#{number}", options)['issue']
       end
 
+      # Get all comments attached to an issue
+      #
+      # @param repository [String, Repository, Hash] A GitHub repository.
+      # @param number [String] Number ID of the issue
+      # @return [Array] Array of comments that belong to an issue
+      # @see http://develop.github.com/p/issues.html
+      # @example Get comments for issue #25 from pengwynn/octokit
+      #   Octokit.issue_comments("pengwynn/octokit", "25")
       def issue_comments(repo, number, options={})
         get("api/v2/json/issues/comments/#{Repository.new(repo)}/#{number}", options)['comments']
       end
 
+      # Close an issue
+      #
+      # @param repository [String, Repository, Hash] A GitHub repository.
+      # @param number [String] Number ID of the issue
+      # @return [Issue] The updated Issue
+      # @see http://develop.github.com/p/issues.html
+      # @note This implementation needs to be adjusted with switch to API v3
+      # @see http://developer.github.com/v3/issues/#edit-an-issue 
+      # @example Close Issue #25 from pengwynn/octokit
+      #   Octokit.close_issue("pengwynn/octokit", "25")
       def close_issue(repo, number, options={})
         post("api/v2/json/issues/close/#{Repository.new(repo)}/#{number}", options)['issue']
       end
 
+      # Reopen an issue
+      #
+      # @param repository [String, Repository, Hash] A GitHub repository.
+      # @param number [String] Number ID of the issue
+      # @return [Issue] The updated Issue
+      # @see http://develop.github.com/p/issues.html
+      # @note This implementation needs to be adjusted with switch to API v3
+      # @see http://developer.github.com/v3/issues/#edit-an-issue 
+      # @example Reopen Issue #25 from pengwynn/octokit
+      #   Octokit.reopen_issue("pengwynn/octokit", "25")
       def reopen_issue(repo, number, options={})
         post("api/v2/json/issues/reopen/#{Repository.new(repo)}/#{number}", options)['issue']
       end
 
+      # Update an issue
+      #
+      # @param repository [String, Repository, Hash] A GitHub repository.
+      # @param number [String] Number ID of the issue
+      # @param title [String] Updated title for the issue
+      # @param body [String] Updated body of the issue
+      # @return [Issue] The updated Issue
+      # @see http://develop.github.com/p/issues.html
+      # @note This implementation needs to be adjusted with switch to API v3
+      # @see http://developer.github.com/v3/issues/#edit-an-issue 
+      # @example Change the title of Issue #25 from pengwynn/octokit
+      #   Octokit.update_issue("pengwynn/octokit", "25", "A new title", "the same body"")
       def update_issue(repo, number, title, body, options={})
         post("api/v2/json/issues/edit/#{Repository.new(repo)}/#{number}", options.merge({:title => title, :body => body}))['issue']
       end
@@ -96,3 +136,4 @@ module Octokit
     end
   end
 end
+
