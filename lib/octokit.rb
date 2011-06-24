76a1c13c931b68b21e39556dcbb12fed8ddd7e10
Erik Michaels-Ober <sferik@gmail.com>
2011-06-23 19:39:26 -0700

    Organize errors into a separate file

--- lib/octokit.rb
+++ lib/octokit.rb
@@ -1,56 +1,25 @@
 require 'octokit/configuration'
 require 'octokit/client'
+require 'octokit/error'
 
 module Octokit
   extend Configuration
+  class << self
+    # Alias for Octokit::Client.new
+    #
+    # @return [Octokit::Client]
+    def client(options={})
+      Octokit::Client.new(options)
+    end
 
-  # Alias for Octokit::Client.new
-  #
-  # @return [Octokit::Client]
-  def self.client(options={})
-    Octokit::Client.new(options)
+    # Delegate to Octokit::Client.new
+    def method_missing(method, *args, &block)
+      return super unless client.respond_to?(method)
+      client.send(method, *args, &block)
+    end
+
+    def respond_to?(method, include_private=false)
+      client.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
-
-  # Delegate to Octokit::Client.new
-  def self.method_missing(method, *args, &block)
-    return super unless client.respond_to?(method)
-    client.send(method, *args, &block)
-  end
-
-  def self.respond_to?(method, include_private=false)
-    client.respond_to?(method, include_private) || super(method, include_private)
-  end
-
-  # Custom error class for rescuing from all GitHub errors
-  class Error < StandardError; end
-
-  # Raised when GitHub returns a 400 HTTP status code
-  class BadRequest < Error; end
-
-  # Raised when GitHub returns a 401 HTTP status code
-  class Unauthorized < Error; end
-
-  # Raised when GitHub returns a 403 HTTP status code
-  class Forbidden < Error; end
-
-  # Raised when GitHub returns a 404 HTTP status code
-  class NotFound < Error; end
-
-  # Raised when GitHub returns a 406 HTTP status code
-  class NotAcceptable < Error; end
-
-  # Raised when GitHub returns a 422 HTTP status code
-  class UnprocessableEntity < Error; end
-
-  # Raised when GitHub returns a 500 HTTP status code
-  class InternalServerError < Error; end
-
-  # Raised when GitHub returns a 501 HTTP status code
-  class NotImplemented < Error; end
-
-  # Raised when GitHub returns a 502 HTTP status code
-  class BadGateway < Error; end
-
-  # Raised when GitHub returns a 503 HTTP status code
-  class ServiceUnavailable < Error; end
 end
