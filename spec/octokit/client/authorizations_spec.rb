6a0c670af135010a5b819003038a40034be2786e
Clint Shryock <clint@ctshryock.com>
2012-05-16 08:25:48 -0500

    Add Authorization spec template, Authorizations fixture file
    Spec for listing authorizations, stubbed out some other things to have tests for

---
+++ spec/octokit/client/authorizations_spec.rb
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+require 'helper'
+
+describe Octokit::Client::Authorizations do
+
+  before do
+    @client = Octokit::Client.new(:login => 'ctshryock', :password => 'secret')
+  end
+
+
+  it "should list existing authorizations" do
+    stub_get("/authorizations").
+    to_return(:body => fixture("v3/authorizations.json"))
+    authorizations = @client.authorizations
+    authorizations.first.app.name.should == "Calendar About Nothing" 
+  end
+
+  it "should return a single authorization" do
+
+  end
+
+  it "should create a new authorization" do
+
+  end
+
+  it "should update and existing authorization" do
+
+  end
+
+  it "should delete an existing authorization" do
+
+  end
+
+  it "should scope permissions correctly" do
+
+  end
+
+  describe "restrictions" do
+
+    it "should work with basic auth" do
+
+    end
+
+    it "should raise an error when used with OAuth" do
+
+    end
+
+  end
+
+end
