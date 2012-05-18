47deabdee1cf01907a123773d8ed7117b3b8ac99
Clint Shryock <clint@ctshryock.com>
2012-05-17 22:32:24 -0500

    Add `create_authorization` method, specs
    Filled out spec to get a single auth too

--- lib/octokit/client/authorizations.rb
+++ lib/octokit/client/authorizations.rb
@@ -18,9 +18,8 @@ module Octokit
       end
 
 
-      # Get a single authorization      
+      # Get a single authorization for the authenticated user.
       #
-      # Get a single authorization for the authenticated user
       # You can only access your own tokens, and only through 
       # Basic Authentication.
       #
@@ -31,6 +30,27 @@ module Octokit
       #  client.authorization(999999)
       def authorization(number)
         get("/authorizations/#{number}")
+      end
+
+      # Create an authorization for the authenticated user.
+      #
+      # You can create your own tokens, and only through 
+      # Basic Authentication.
+      #
+      # @param options [Hash] A customizable set of options.
+      # @option options [Array] :scopes A list of scopes that this authorization is in.
+      #   See http://developer.github.com/v3/oauth/#scopes for available scopes
+      # @option options [String] :note A note to remind you what the OAuth token is for.
+      # @option options [String] :note_url A URL to remind you what app the OAuth token is for.
+      #
+      # @return [Authorization] A single authorization for the authenticated user
+      # @see http://developer.github.com/v3/oauth/#create-a-new-authorization
+      # @example Create a new authorization for user ctshryock's project Zoidberg
+      #  client = Octokit::Client.new(:login => 'ctshryock', :password => 'secret')
+      #  client.create_authorization({:scopes => ["public_repo","gist"], :note => "Why not Zoidberg?", :note_url=> "https://en.wikipedia.org/wiki/Zoidberg"})
+      def create_authorization(options={})
+        options = {:scopes => ""}.merge(options)
+        post('/authorizations', options)
       end
 
     end
--- spec/octokit/client/authorizations_spec.rb
+++ spec/octokit/client/authorizations_spec.rb
@@ -10,20 +10,34 @@ describe Octokit::Client::Authorizations do
 
   it "should list existing authorizations" do
     stub_get("/authorizations").
-    to_return(:body => fixture("v3/authorizations.json"))
+      to_return(:body => fixture("v3/authorizations.json"))
     authorizations = @client.authorizations
     authorizations.first.app.name.should == "Calendar About Nothing" 
   end
 
   it "should return a single authorization" do
     stub_get("/authorizations/999999").
-    to_return(:body => fixture("v3/authorization.json"))
-    authorizations = @client.authorization(999999)
-    authorizations.app.name.should == "Travis" 
+      to_return(:body => fixture("v3/authorization.json"))
+    authorization = @client.authorization(999999)
+    authorization.app.name.should == "Travis" 
   end
 
-  it "should create a new authorization" do
+  it "should create a new default authorization" do
+    stub_post('/authorizations').
+      with(:body => {"scopes" => ""},
+           :headers => {'Content-Type'=>'application/json'}).
+      to_return(:body => fixture("v3/authorization.json"))
+    authorization = @client.create_authorization
+    authorization.app.name.should == "Travis" 
+  end
 
+  it "should create a new authorization with options" do
+    stub_post('/authorizations').
+      with(:body => {"scopes" => ["public_repo"],"note" => "admin script", "note_url" => "https://github.com/pengwynn/octokit"},
+           :headers => {'Content-Type'=>'application/json'}).
+      to_return(:body => fixture("v3/authorization.json"))
+    authorization = @client.create_authorization({:scopes => ["public_repo"], :note => "admin script", :note_url => "https://github.com/pengwynn/octokit"})
+    authorization.scopes.should include("public_repo")
   end
 
   it "should update and existing authorization" do
@@ -31,10 +45,6 @@ describe Octokit::Client::Authorizations do
   end
 
   it "should delete an existing authorization" do
-
-  end
-
-  it "should scope permissions correctly" do
 
   end
 
