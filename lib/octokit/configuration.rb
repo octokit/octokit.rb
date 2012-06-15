8099a0ef5e947e7294cf4a8431685da5111a0930
Wynn Netherland <wynn.netherland@gmail.com>
2012-06-15 15:31:09 -0500

    Configurable pubsubhubbub urls

--- lib/octokit/configuration.rb
+++ lib/octokit/configuration.rb
@@ -19,7 +19,7 @@ module Octokit
     DEFAULT_ADAPTER        = Faraday.default_adapter
     DEFAULT_API_VERSION    = 3
     DEFAULT_API_ENDPOINT   = 'https://api.github.com'
-    DEFAULT_WEB_ENDPOINT   = 'https://github.com/'
+    DEFAULT_WEB_ENDPOINT   = 'https://github.com'
     DEFAULT_USER_AGENT     = "Octokit Ruby Gem #{Octokit::VERSION}".freeze
     DEFAULT_AUTO_TRAVERSAL = false
 
