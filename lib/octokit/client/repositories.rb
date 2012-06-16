e8a858720be33385bdaf87a4f130d7d5b3853c7a
Will <me@willrax.com>
2012-06-16 19:24:18 +1000

    added delete repo

--- lib/octokit/client/repositories.rb
+++ lib/octokit/client/repositories.rb
@@ -54,6 +54,12 @@ module Octokit
       alias :create_repo :create_repository
       alias :create :create_repository
 
+      def delete_repository(name, options={})
+        delete "repos/user/#{Repository.new repo}"
+      end
+      alias :delete :delete_repository
+      alias :delete_repo, :delete_repository
+
       def set_private(repo, options={})
         update_repository repo, options.merge({ :private => true })
       end
