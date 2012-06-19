6201556c9dcdd24b95c4ca3c23c8be1b07046bfc
willrax <me@willrax.com>
2012-06-20 07:02:23 +1000

    refactored some options params

--- lib/octokit/client/contents.rb
+++ lib/octokit/client/contents.rb
@@ -6,31 +6,26 @@ module Octokit
       #
       # @param repo [String, Repository, Hash] A GitHub repository
       # @param ref [String] The String name of the Commit/Branch/Tag. Defaults to “master”.
+      # @option options [String] :ref name of the Commit/Branch/Tag. Defaults to “master”.
       # @return [Hash] The detail of the readme
       # @see http://developer.github.com/v3/repos/contents/
       # @example Get the readme file for a repo
       #   Octokit.readme("pengwynn/octokit")
-			def readme(repo, ref="master", options={})
-				 parameters = {
-          :ref  => ref,
-        }
-				get("/repos/#{Repository.new repo}/readme", options.merge(parameters), 3)
+			def readme(repo, options={})
+				get("/repos/#{Repository.new repo}/readme", options, 3)
 			end
 
       # Receive a listing of a repository folder or the contents of a file
       #
       # @param repo [String, Repository, Hash] A GitHub repository
       # @param path [String] A folder or file path
-      # @param ref [String] The String name of the Commit/Branch/Tag. Defaults to “master”.
+      # @option options [String] :ref name of the Commit/Branch/Tag. Defaults to “master”.
       # @return [Hash] The contents of a file or list of the files in the folder
       # @see http://developer.github.com/v3/repos/contents/
       # @example List the contents of /lib/octokit.rb
       #   Octokit.contents("pengwynn/octokit", 'lib/octokit.rb')
-			def contents(repo, path="", ref="master", options={})
-				parameters = {
-          :ref  => ref,
-        }
-				get("/repos/#{Repository.new repo}/contents/#{path}", options.merge(parameters), 3)
+			def contents(repo, path="", options={})
+				get("/repos/#{Repository.new repo}/contents/#{path}", options, 3)
 			end
 			
 			# This method will provide a URL to download a tarball or zipball archive for a repository. 
@@ -42,7 +37,7 @@ module Octokit
       # @see http://developer.github.com/v3/repos/contents/
       # @example Get archive link for pengwynn/octokit
       #   Octokit.archive_link("pengwynn/octokit")
-			def archive_link(repo, archive_format="tarball", ref="master", options={})
+			def archive_link(repo, archive_format="tarball", ref="", options={})
 				get("/repos/#{Repository.new repo}/#{archive_format}/#{ref}", options, 3, false, true).headers
 			end
 
--- spec/octokit/client/contents_spec.rb
+++ spec/octokit/client/contents_spec.rb
@@ -10,7 +10,7 @@ describe Octokit::Client::Contents do
 	describe ".readme" do
 
 		it "should return the default readme" do 
-			stub_get("/repos/pengwynn/octokit/readme?ref=master").
+			stub_get("/repos/pengwynn/octokit/readme").
 				to_return(:body => fixture("v3/readme.json"))
 			readme = @client.readme('pengwynn/octokit')
 			readme.encoding.should == "base64"
@@ -22,7 +22,7 @@ describe Octokit::Client::Contents do
 	describe ".contents" do
 
 		it "should return the contents of a file" do
-			stub_get("/repos/pengwynn/octokit/contents/lib/octokit.rb?ref=master").
+			stub_get("/repos/pengwynn/octokit/contents/lib/octokit.rb").
 				to_return(:body => fixture("v3/contents.json"))
 			contents = @client.contents('pengwynn/octokit', 'lib/octokit.rb')
 			contents.path.should == "lib/octokit.rb"
@@ -36,11 +36,11 @@ describe Octokit::Client::Contents do
 	describe ".archive_link" do
 
 		it "should return the location of the archive from get header" do
-			stub_get("/repos/pengwynn/octokit/tarball/master").
+			stub_get("/repos/pengwynn/octokit/tarball/").
 				to_return(:status => 302, :body => '', :headers => 
-					{ 'location' => "https://nodeload.github.com/pengwynn/octokit/tarball/master"})
+					{ 'location' => "https://nodeload.github.com/repos/pengwynn/octokit/tarball/"})
 			archive_link = @client.archive_link('pengwynn/octokit')
-			archive_link['location'] == "https://nodeload.github.com/pengwynn/octokit/tarball/master"
+			archive_link['location'] == "https://nodeload.github.com/pengwynn/octokit/tarball/"
 		end
 
 	end
