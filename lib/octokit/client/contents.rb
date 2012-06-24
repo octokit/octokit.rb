668ba53fc8be0cce97d0ea27e76d04860083faed
willrax <me@willrax.com>
2012-06-24 15:50:14 +1000

    refactored and works in gem, but tests fail

--- lib/octokit/client/contents.rb
+++ lib/octokit/client/contents.rb
@@ -2,45 +2,46 @@ module Octokit
 	class Client
 		module Contents
 
-      # Receive the default Readme for a repository
-      #
-      # @param repo [String, Repository, Hash] A GitHub repository
-      # @param ref [String] The String name of the Commit/Branch/Tag. Defaults to “master”.
-      # @option options [String] :ref name of the Commit/Branch/Tag. Defaults to “master”.
-      # @return [Hash] The detail of the readme
-      # @see http://developer.github.com/v3/repos/contents/
-      # @example Get the readme file for a repo
-      #   Octokit.readme("pengwynn/octokit")
+	# Receive the default Readme for a repository
+	#
+	# @param repo [String, Repository, Hash] A GitHub repository
+	# @param ref [String] The String name of the Commit/Branch/Tag. Defaults to “master”.
+	# @option options [String] :ref name of the Commit/Branch/Tag. Defaults to “master”.
+	# @return [Hash] The detail of the readme
+	# @see http://developer.github.com/v3/repos/contents/
+	# @example Get the readme file for a repo
+	#   Octokit.readme("pengwynn/octokit")
 			def readme(repo, options={})
 				get("/repos/#{Repository.new repo}/readme", options, 3)
 			end
 
-      # Receive a listing of a repository folder or the contents of a file
-      #
-      # @param repo [String, Repository, Hash] A GitHub repository
-      # @param path [String] A folder or file path
-      # @option options [String] :ref name of the Commit/Branch/Tag. Defaults to “master”.
-      # @return [Hash] The contents of a file or list of the files in the folder
-      # @see http://developer.github.com/v3/repos/contents/
-      # @example List the contents of /lib/octokit.rb
-      #   Octokit.contents("pengwynn/octokit", 'lib/octokit.rb')
-			def contents(repo, path="", options={})
-				get("/repos/#{Repository.new repo}/contents/#{path}", options, 3)
+	# Receive a listing of a repository folder or the contents of a file
+	#
+	# @param repo [String, Repository, Hash] A GitHub repository
+	# @option options [String] :path A folder or file path
+	# @option options [String] :ref name of the Commit/Branch/Tag. Defaults to “master”.
+	# @return [Hash] The contents of a file or list of the files in the folder
+	# @see http://developer.github.com/v3/repos/contents/
+	# @example List the contents of lib/octokit.rb
+	#   Octokit.contents("pengwynn/octokit", :path => 'lib/octokit.rb')
+			def contents(repo, options={})
+				repo_path, repo_ref = options.delete("path"), options.delete("ref")
+				get("/repos/#{Repository.new repo}/contents/#{repo_path}/#{repo_ref}", options, 3)
 			end
 			
-			# This method will provide a URL to download a tarball or zipball archive for a repository. 
-      #
-      # @param repo [String, Repository, Hash] A GitHub repository.
-      # @param archive_format [String] Either tarball or zipball.
-      # @param ref [String] Optional valid Git reference, defaults to master.
-      # @return [Faraday::Utils::Headers] The header of the response.
-      # @see http://developer.github.com/v3/repos/contents/
-      # @example Get archive link for pengwynn/octokit
-      #   Octokit.archive_link("pengwynn/octokit")
-			def archive_link(repo, archive_format="tarball", ref="", options={})
-				get("/repos/#{Repository.new repo}/#{archive_format}/#{ref}", options, 3, false, true).headers
-			end
-
-		end
+	# This method will provide a URL to download a tarball or zipball archive for a repository. 
+	#
+	# @param repo [String, Repository, Hash] A GitHub repository.
+	# @param archive_format [String] Either tarball (default) or zipball.
+	# @option options [String] :ref Optional valid Git reference, defaults to master.
+	# @return [Faraday::Utils::Headers] The header of the response.
+	# @see http://developer.github.com/v3/repos/contents/
+	# @example Get archive link for pengwynn/octokit
+	#   Octokit.archive_link("pengwynn/octokit")
+			def archive_link(repo, archive_format="tarball", options={})
+				url_to_get = "/repos/#{Repository.new repo}/#{archive_format}/#{options.delete("ref")}"
+			  get(url_to_get, options, 3, false, true).headers
+		  end
 	end
+end
 end
--- spec/octokit/client/contents_spec.rb
+++ spec/octokit/client/contents_spec.rb
@@ -24,7 +24,7 @@ describe Octokit::Client::Contents do
 		it "should return the contents of a file" do
 			stub_get("/repos/pengwynn/octokit/contents/lib/octokit.rb").
 				to_return(:body => fixture("v3/contents.json"))
-			contents = @client.contents('pengwynn/octokit', 'lib/octokit.rb')
+			contents = @client.contents('pengwynn/octokit', :path => "lib/octokit.rb")
 			contents.path.should == "lib/octokit.rb"
 			contents.name.should == "lib/octokit.rb"
 			contents.encoding.should == "base64"
@@ -35,11 +35,11 @@ describe Octokit::Client::Contents do
 
 	describe ".archive_link" do
 
-		it "should return the location of the archive from get header" do
-			stub_get("/repos/pengwynn/octokit/tarball/").
+		it "should return the headers of the request" do
+			stub_get("/repos/pengwynn/octokit/tarball/master").
 				to_return(:status => 302, :body => '', :headers => 
 					{ 'location' => "https://nodeload.github.com/repos/pengwynn/octokit/tarball/"})
-			archive_link = @client.archive_link('pengwynn/octokit')
+			archive_link = @client.archive_link('pengwynn/octokit', :ref => "master")
 			archive_link['location'] == "https://nodeload.github.com/pengwynn/octokit/tarball/"
 		end
 
