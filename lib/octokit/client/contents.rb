b5451c06a5efa64f742d9335cec680746713b185
Wynn Netherland <wynn@github.com>
2013-05-06 16:33:51 -0500

    Implement C-U-D for Contents API

--- lib/octokit/client/contents.rb
+++ lib/octokit/client/contents.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module Octokit
   class Client
     module Contents
@@ -28,6 +30,79 @@ module Octokit
         url = "repos/#{Repository.new repo}/contents/#{repo_path}"
         get(url, options)
       end
+      alias :content :contents
+
+      # Add content to a repository
+      #
+      # @param repo [String, Repository, Hash] A GitHub repository
+      # @param path [String] A path for the new content
+      # @param message [String] A commit message for adding the content
+      # @param content [String] The Base64-encoded content for the file
+      # @option options [String] :branch The branch on which to add the content
+      # @return [Hash] The contents and commit info for the addition
+      # @see http://developer.github.com/v3/repos/contents/#create-a-file
+      # @example Add content at lib/octokit.rb
+      #   Octokit.create_contents("pengwynn/octokit",
+      #                    "lib/octokit.rb",
+      #                    "Adding content",
+      #                    "asdf9as0df9asdf8as0d9f8==...",
+      #                    :branch => "my-new-feature")
+      def create_contents(repo, path, message, content, options = {})
+        options[:content] = Base64.encode64(content)
+        options[:message] = message
+        url = "repos/#{Repository.new repo}/contents/#{path}"
+        put(url, options)
+      end
+      alias :create_content :create_contents
+      alias :add_content :create_contents
+      alias :add_contents :create_contents
+
+      # Update content in a repository
+      #
+      # @param repo [String, Repository, Hash] A GitHub repository
+      # @param path [String] A path for the content to update
+      # @param message [String] A commit message for updating the content
+      # @param content [String] The Base64-encoded content for the file
+      # @param sha [String] The _blob sha_ of the content to update
+      # @option options [String] :branch The branch on which to update the content
+      # @return [Hash] The contents and commit info for the update
+      # @see http://developer.github.com/v3/repos/contents/#update-a-file
+      # @example Update content at lib/octokit.rb
+      #   Octokit.update_contents("pengwynn/octokit",
+      #                    "lib/octokit.rb",
+      #                    "Updating content",
+      #                    "asdf9as0df9asdf8as0d9f8==...",
+      #                    "7eb95f97e1a0636015df3837478d3f15184a5f49",
+      #                    :branch => "my-new-feature")
+      def update_contents(repo, path, message, content, sha, options = {})
+        create_contents(repo, path, message, content, :sha => sha)
+      end
+      alias :update_content :update_contents
+
+      # Delete content in a repository
+      #
+      # @param repo [String, Repository, Hash] A GitHub repository
+      # @param path [String] A path for the content to delete
+      # @param message [String] A commit message for deleting the content
+      # @param sha [String] The _blob sha_ of the content to delete
+      # @option options [String] :branch The branch on which to delete the content
+      # @return [Hash] The commit info for the delete
+      # @see http://developer.github.com/v3/repos/contents/#delete-a-file
+      # @example Delete content at lib/octokit.rb
+      #   Octokit.delete_contents("pengwynn/octokit",
+      #                    "lib/octokit.rb",
+      #                    "Deleting content",
+      #                    "7eb95f97e1a0636015df3837478d3f15184a5f49",
+      #                    :branch => "my-new-feature")
+      def delete_contents(repo, path, message, sha, options = {})
+        options[:message] = message
+        options[:sha] = sha
+        url = "repos/#{Repository.new repo}/contents/#{path}"
+        delete(url, options)
+      end
+      alias :delete_content :delete_contents
+      alias :remove_content :delete_contents
+      alias :remove_contents :delete_contents
 
       # This method will provide a URL to download a tarball or zipball archive for a repository.
       #
