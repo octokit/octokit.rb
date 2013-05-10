4cab9afe1bd0efe6a38fbf357b48778f06996f89
Wynn Netherland <wynn@github.com>
2013-05-10 08:45:45 -0500

    Refactor Commit Comments specs to be refreshable

--- spec/octokit/client/commit_comments_spec.rb
+++ spec/octokit/client/commit_comments_spec.rb
@@ -4,7 +4,13 @@ describe Octokit::Client::CommitComments do
 
   before do
     Octokit.reset!
-    VCR.insert_cassette 'commit_comments', :match_requests_on => [:method, :uri, :query]
+    VCR.insert_cassette 'commit_comments', :match_requests_on => [:method, :uri, :query, :body]
+    @client = basic_auth_client
+    @commit = @client.commits('api-playground/api-sandbox').last.rels[:self].get.data
+    @commit_comment = @client.create_commit_comment \
+      "api-playground/api-sandbox",
+      @commit.sha, ":metal:\n:sparkles:\n:cake:",
+      @commit.files.last.filename
   end
 
   after do
@@ -37,27 +43,25 @@ describe Octokit::Client::CommitComments do
 
   describe ".create_commit_comment" do
     it "creates a commit comment" do
-      client = basic_auth_client
-      commit_comment = client.create_commit_comment("api-playground/api-sandbox", "eb11b3141c9dec3ba88d15b499d597a65df15320", ":metal:\n:sparkles:\n:cake:", ".todo")
-      expect(commit_comment.user.login).to eq test_github_login
-      assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/commits/eb11b3141c9dec3ba88d15b499d597a65df15320/comments")
+      expect(@commit_comment.user.login).to eq test_github_login
+      assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/commits/#{@commit.sha}/comments")
     end
   end # .create_commit_comment
 
   describe ".update_commit_comment" do
     it "updates a commit comment" do
-      client = basic_auth_client
-      commit_comment = client.update_commit_comment("api-playground/api-sandbox", 3132501, ":penguin:")
-      assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/comments/3132501")
+      updated_comment = @client.update_commit_comment("api-playground/api-sandbox", @commit_comment.id, ":penguin:")
+      expect(updated_comment.body).to eq ":penguin:"
+      assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/comments/#{@commit_comment.id}")
     end
   end # .update_commit_comment
 
   describe ".delete_commit_comment" do
     it "deletes a commit comment" do
-      client = basic_auth_client
-      result = client.delete_commit_comment("api-playground/api-sandbox", 3132501)
+      result = @client.delete_commit_comment("api-playground/api-sandbox", @commit_comment.id)
       expect(result).to be_true
-      assert_requested :delete, basic_github_url("/repos/api-playground/api-sandbox/comments/3132501")
+      assert_requested :delete, basic_github_url("/repos/api-playground/api-sandbox/comments/#{@commit_comment.id}")
     end
   end # .delete_commit_comment
+
 end
