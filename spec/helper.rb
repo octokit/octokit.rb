aa2b4222daae890a096d57ad6f16a8e0bdf78083
Wynn Netherland <wynn@github.com>
2013-05-10 08:39:47 -0500

    Ordered tests are required for sane VCR tests

--- spec/helper.rb
+++ spec/helper.rb
@@ -15,7 +15,7 @@ WebMock.disable_net_connect!(:allow => 'coveralls.io')
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.order = 'random'
+  config.order = :default
 end
 
 require 'vcr'
