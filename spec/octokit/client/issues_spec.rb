f103bb179393beaa37e500449eb44e89091a9341
Wynn Netherland <wynn@github.com>
2013-05-10 10:58:35 -0500

    Organize Issues API specs to be re-runnable with empty cassettes

--- spec/octokit/client/issues_spec.rb
+++ spec/octokit/client/issues_spec.rb
@@ -4,7 +4,8 @@ describe Octokit::Client::Issues do
 
   before do
     Octokit.reset!
-    VCR.insert_cassette 'issues', :match_requests_on => [:method, :uri, :body]
+    VCR.insert_cassette 'issues', :match_requests_on => [:method, :uri, :body, :headers]
+    @client = basic_auth_client
   end
 
   after do
@@ -19,8 +20,7 @@ describe Octokit::Client::Issues do
       assert_requested :get, github_url("/repos/sferik/rails_admin/issues")
     end
     it "returns dashboard issues for the authenticated user" do
-      client = basic_auth_client
-      issues = client.issues
+      issues = @client.issues
       expect(issues).to be_kind_of Array
       assert_requested :get, basic_github_url("/issues")
     end
@@ -28,8 +28,7 @@ describe Octokit::Client::Issues do
 
   describe ".user_issues" do
     it "returns issues for the authenticated user for owned and member repos" do
-      client = basic_auth_client
-      issues = client.user_issues
+      issues = @client.user_issues
       expect(issues).to be_kind_of Array
       assert_requested :get, basic_github_url("/user/issues")
     end
@@ -37,8 +36,7 @@ describe Octokit::Client::Issues do
 
   describe ".org_issues" do
     it "returns issues for the organization for the authenticated user" do
-      client = basic_auth_client
-      issues = client.org_issues('dotfiles')
+      issues = @client.org_issues('dotfiles')
       expect(issues).to be_kind_of Array
       assert_requested :get, basic_github_url("/orgs/dotfiles/issues")
     end
@@ -46,55 +44,87 @@ describe Octokit::Client::Issues do
 
   describe ".create_issue" do
     it "creates an issue" do
-      client = basic_auth_client
-      issue = client.create_issue("pengwynn/api-sandbox", "Migrate issues to v3", "Move all Issues calls to v3 of the API")
+      issue = @client.create_issue("api-playground/api-sandbox", "Migrate issues to v3", "Move all Issues calls to v3 of the API")
       expect(issue.title).to match /Migrate/
-      assert_requested :post, basic_github_url("/repos/pengwynn/api-sandbox/issues")
+      assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/issues")
     end
   end # .create_issue
 
-  describe ".issue" do
-    it "returns an issue" do
-      client = basic_auth_client
-      issue = client.issue("pengwynn/api-sandbox", 22)
-      assert_requested :get, basic_github_url("/repos/pengwynn/api-sandbox/issues/22")
-      expect(issue.number).to eq 22
-    end
-    it "returns a full issue" do
-      client = basic_auth_client
-      issue = client.issue("pengwynn/api-sandbox", 22, :accept => 'application/vnd.github.full+json')
-      assert_requested :get, basic_github_url("/repos/pengwynn/api-sandbox/issues/22")
-      expect(issue.body_html).to include '<p>Move all'
-      expect(issue.body_text).to include 'Move all'
-    end
-  end # .issue
+  context "methods requiring a new issue" do
 
-  describe ".close_issue" do
-    it "closes an issue" do
-      client = basic_auth_client
-      issue = client.close_issue("pengwynn/api-sandbox", 22)
-      expect(issue.number).to eq 22
-      assert_requested :patch, basic_github_url("/repos/pengwynn/api-sandbox/issues/22")
+    before do
+      @issue = @client.create_issue("api-playground/api-sandbox", "Migrate issues to v3", "Move all Issues calls to v3 of the API")
     end
-  end # .close_issue
 
-  describe ".reopen_issue" do
-    it "reopens an issue" do
-      client = basic_auth_client
-      issue = client.close_issue("pengwynn/api-sandbox", 22)
-      expect(issue.number).to eq 22
-      assert_requested :patch, basic_github_url("/repos/pengwynn/api-sandbox/issues/22")
-    end
-  end # .reopen_issue
+    describe ".issue" do
+      it "returns an issue" do
+        issue = @client.issue("api-playground/api-sandbox", @issue.number)
+        assert_requested :get, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}")
+        expect(issue.number).to eq @issue.number
+      end
+      it "returns a full issue" do
+        issue = @client.issue("api-playground/api-sandbox", @issue.number, :accept => 'application/vnd.github.full+json')
+        assert_requested :get, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}")
+        expect(issue.body_html).to include '<p>Move all'
+        expect(issue.body_text).to include 'Move all'
+      end
+    end # .issue
 
-  describe ".update_issue" do
-    it "updates an issue" do
-      client = basic_auth_client
-      issue = client.update_issue("pengwynn/api-sandbox", 22, "Use all the v3 api!", "")
-      expect(issue.number).to eq 22
-      assert_requested :patch, basic_github_url("/repos/pengwynn/api-sandbox/issues/22")
+    describe ".close_issue" do
+      it "closes an issue" do
+        issue = @client.close_issue("api-playground/api-sandbox", @issue.number)
+        expect(issue.number).to eq @issue.number
+        assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}")
+      end
+    end # .close_issue
+
+    describe ".reopen_issue" do
+      it "reopens an issue" do
+        issue = @client.close_issue("api-playground/api-sandbox", @issue.number)
+        expect(issue.number).to eq @issue.number
+        assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}")
+      end
+    end # .reopen_issue
+
+    describe ".update_issue" do
+      it "updates an issue" do
+        issue = @client.update_issue("api-playground/api-sandbox", @issue.number, "Use all the v3 api!", "")
+        expect(issue.number).to eq @issue.number
+        assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}")
+      end
+    end # .update_issue
+
+    describe ".add_comment" do
+      it "adds a comment" do
+        comment = @client.add_comment("api-playground/api-sandbox", @issue.number, "A test comment")
+        expect(comment.user.login).to eq test_github_login
+        assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}/comments")
+      end
+    end # .add_comment
+
+    context "methods requiring a new issue comment" do
+
+      before do
+        @issue_comment = @client.add_comment("api-playground/api-sandbox", @issue.number, "Another test comment")
+      end
+
+      describe ".update_comment" do
+        it "updates an existing comment" do
+          @client.update_comment("api-playground/api-sandbox", @issue_comment.id, "A test comment update")
+          assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/issues/comments/#{@issue_comment.id}")
+        end
+      end # .update_comment
+
+      describe ".delete_comment" do
+        it "deletes an existing comment" do
+          @client.delete_comment("api-playground/api-sandbox", @issue_comment.id)
+          assert_requested :delete, basic_github_url("/repos/api-playground/api-sandbox/issues/comments/#{@issue_comment.id}")
+        end # .delete_comment
+      end
+
     end
-  end # .update_issue
+
+  end
 
   describe ".repository_issues_comments" do
     it "returns comments for all issues in a repository" do
@@ -119,32 +149,5 @@ describe Octokit::Client::Issues do
       assert_requested :get, github_url('/repos/pengwynn/octokit/issues/comments/1194690')
     end
   end # .issue_comment
-
-  describe ".add_comment" do
-    it "adds a comment" do
-      client = basic_auth_client
-      comment = client.add_comment("pengwynn/api-sandbox", 22, "A test comment")
-      expect(comment.user.login).to eq test_github_login
-      assert_requested :post, basic_github_url("/repos/pengwynn/api-sandbox/issues/22/comments")
-    end
-  end # .add_comment
-
-  describe ".update_comment" do
-    it "updates an existing comment" do
-      client = basic_auth_client
-      comment = client.add_comment("pengwynn/api-sandbox", 22, "Another test comment")
-      client.update_comment("pengwynn/api-sandbox", comment.id, "A test comment update")
-      assert_requested :patch, basic_github_url("/repos/pengwynn/api-sandbox/issues/comments/#{comment.id}")
-    end
-  end # .update_comment
-
-  describe ".delete_comment" do
-    it "deletes an existing comment" do
-      client = basic_auth_client
-      comment = client.add_comment("pengwynn/api-sandbox", 22, "Another test comment")
-      client.delete_comment("pengwynn/api-sandbox", comment.id)
-      assert_requested :delete, basic_github_url("/repos/pengwynn/api-sandbox/issues/comments/#{comment.id}")
-    end # .delete_comment
-  end
 
 end
