ce06316df15e68b7c75c7bf1398b33afdef99510
Wynn Netherland <wynn@github.com>
2013-05-10 11:25:57 -0500

    Reorganize Labels API specs to be runnable with empty VCR

--- spec/octokit/client/labels_spec.rb
+++ spec/octokit/client/labels_spec.rb
@@ -5,6 +5,7 @@ describe Octokit::Client::Labels do
   before do
     Octokit.reset!
     VCR.insert_cassette 'labels'
+    @client = basic_auth_client
   end
 
   after do
@@ -30,63 +31,72 @@ describe Octokit::Client::Labels do
 
   describe ".add_label" do
     it "adds a label with a color" do
-      client = basic_auth_client
-      label = client.add_label("pengwynn/api-sandbox", "test-label", 'ededed')
+      label = @client.add_label("api-playground/api-sandbox", "test-label", 'ededed')
       expect(label.color).to eq "ededed"
-      assert_requested :post, basic_github_url("/repos/pengwynn/api-sandbox/labels")
+      assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/labels")
     end
     it "adds a label with default color" do
-      client = basic_auth_client
-      label = client.add_label("pengwynn/api-sandbox", "test-label")
-      assert_requested :post, basic_github_url("/repos/pengwynn/api-sandbox/labels")
+      label = @client.add_label("api-playground/api-sandbox", "test-label")
+      assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/labels")
     end
   end # .add_label
 
-  describe ".update_label" do
-    it "updates a label with a new color" do
-      client = basic_auth_client
-      label = client.add_label("pengwynn/api-sandbox", "test-label")
-      client.update_label("pengwynn/api-sandbox", label.name, {:color => 'ededed'})
-      assert_requested :patch, basic_github_url("/repos/pengwynn/api-sandbox/labels/#{label.name}")
-    end
-  end # .update_label
+  context "methods requiring a new label" do
 
-  describe ".add_labels_to_an_issue" do
-    it "adds labels to a given issue" do
-      client = basic_auth_client
-      issue = client.create_issue("pengwynn/api-sandbox", "Issue for label test", "The body")
-      labels = client.add_labels_to_an_issue('pengwynn/api-sandbox', issue.number, ['bug'])
-      assert_requested :post, basic_github_url("/repos/pengwynn/api-sandbox/issues/#{issue.number}" + "/labels")
+    before do
+      @label = @client.add_label("api-playground/api-sandbox", "test-label", 'ededed')
     end
-  end # .add_labels_to_an_issue
 
+    describe ".update_label" do
+      it "updates a label with a new color" do
+        @client.update_label("api-playground/api-sandbox", @label.name, {:color => 'ffdd33'})
+        assert_requested :patch, basic_github_url("/repos/api-playground/api-sandbox/labels/#{@label.name}")
+      end
+    end # .update_label
+  end
 
-  describe ".remove_label" do
-    it "removes a label from the specified issue" do
-      client = basic_auth_client
-      issue = client.create_issue("pengwynn/api-sandbox", "Issue for label test", "The body")
-      labels = client.remove_label('pengwynn/api-sandbox', issue.number, 'bug')
-      assert_requested :delete, basic_github_url("/repos/pengwynn/api-sandbox/issues/#{issue.number}" + "/labels/bug")
+  context "methods requiring a new issue" do
+    before do
+      @issue = @client.create_issue("api-playground/api-sandbox", "Issue for label test", "The body")
     end
-  end # .remove_label
 
-  describe ".remove_all_labels" do
-    it "removes all labels from the specified issue" do
-      client = basic_auth_client
-      issue = client.create_issue("pengwynn/api-sandbox", "Issue for label test", "The body")
-      labels = client.remove_all_labels('pengwynn/api-sandbox', issue.number)
-      assert_requested :delete, basic_github_url("/repos/pengwynn/api-sandbox/issues/#{issue.number}" + "/labels")
-    end
-  end # .remove_all_labels
+    describe ".add_labels_to_an_issue" do
+      it "adds labels to a given issue" do
+        labels = @client.add_labels_to_an_issue('api-playground/api-sandbox', @issue.number, ['bug'])
+        assert_requested :post, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels")
+      end
+    end # .add_labels_to_an_issue
 
-  describe ".replace_all_labels" do
-    it "replaces all labels for an issue" do
-      client = basic_auth_client
-      issue = client.create_issue("pengwynn/api-sandbox", "Issue for label test", "The body")
-      labels = client.replace_all_labels('pengwynn/api-sandbox', issue.number, ['bug', 'pdi'])
-      assert_requested :put, basic_github_url("/repos/pengwynn/api-sandbox/issues/#{issue.number}" + "/labels")
-    end
-  end # .replace_all_labels
+    describe ".labels_for_issue" do
+      it "returns all labels for a given issue" do
+        labels = @client.labels_for_issue('api-playground/api-sandbox', @issue.number)
+        expect(labels).to be_kind_of Array
+        assert_requested :get, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}/labels")
+      end
+    end # .labels_for_issue
+
+    describe ".remove_label" do
+      it "removes a label from the specified issue" do
+        labels = @client.remove_label('api-playground/api-sandbox', @issue.number, 'bug')
+        assert_requested :delete, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels/bug")
+      end
+    end # .remove_label
+
+    describe ".remove_all_labels" do
+      it "removes all labels from the specified issue" do
+        labels = @client.remove_all_labels('api-playground/api-sandbox', @issue.number)
+        assert_requested :delete, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels")
+      end
+    end # .remove_all_labels
+
+    describe ".replace_all_labels" do
+      it "replaces all labels for an issue" do
+        labels = @client.replace_all_labels('api-playground/api-sandbox', @issue.number, ['bug', 'pdi'])
+        assert_requested :put, basic_github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels")
+      end
+    end # .replace_all_labels
+  end
+
 
   describe ".lables_for_milestone" do
     it "returns all labels for a repository" do
@@ -96,20 +106,12 @@ describe Octokit::Client::Labels do
     end
   end # .labels_for_milestone
 
-  describe ".labels_for_issue" do
-    it "returns all labels for a given issue" do
-      labels = Octokit.labels_for_issue('pengwynn/octokit', 37)
-      expect(labels).to be_kind_of Array
-      assert_requested :get, github_url('/repos/pengwynn/octokit/issues/37/labels')
-    end
-  end # .labels_for_issue
 
   describe ".delete_label!" do
     it "deletes a label from the repository" do
-      client = basic_auth_client
-      label = client.add_label("pengwynn/api-sandbox", "test-label")
-      client.delete_label!("pengwynn/api-sandbox", label.name, {:color => 'ededed'})
-      assert_requested :delete, basic_github_url("/repos/pengwynn/api-sandbox/labels/#{label.name}")
+      label = @client.add_label("api-playground/api-sandbox", "delete-me-label")
+      @client.delete_label!("api-playground/api-sandbox", label.name, {:color => 'ededed'})
+      assert_requested :delete, basic_github_url("/repos/api-playground/api-sandbox/labels/#{label.name}")
     end
   end # .delete_label!
 
