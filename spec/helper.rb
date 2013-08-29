2e99273f6e7f49fa39c83f6e7aed2886a7c0cd6b
Jared Pace <jared@codeword.io>
2013-08-29 15:24:22 -0400

    Filter out client_id and secret from VCR cassettes

--- spec/helper.rb
+++ spec/helper.rb
@@ -30,6 +30,12 @@ VCR.configure do |c|
   c.filter_sensitive_data("<<ACCESS_TOKEN>>") do
       ENV['OCTOKIT_TEST_GITHUB_TOKEN']
   end
+  c.filter_sensitive_data("<GITHUB_CLIENT_ID>") do
+      ENV['OCTOKIT_TEST_GITHUB_CLIENT_ID']
+  end
+  c.filter_sensitive_data("<GITHUB_CLIENT_SECRET>") do
+      ENV['OCTOKIT_TEST_GITHUB_CLIENT_SECRET']
+  end
   c.default_cassette_options = {
     :serialize_with             => :json,
     # TODO: Track down UTF-8 issue and remove
