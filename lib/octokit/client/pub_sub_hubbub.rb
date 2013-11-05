fd61bcb9ddb61910a0584311811ab052c6e3d961
Don Morrison <don@elskwid.net>
2013-11-04 20:19:55 -0800

    Add secret support to PubSubHubbub subscribe

--- lib/octokit/client/pub_sub_hubbub.rb
+++ lib/octokit/client/pub_sub_hubbub.rb
@@ -17,16 +17,19 @@ module Octokit
       #
       # @param topic [String] A recoginized and supported pubsub topic
       # @param callback [String] A callback url to be posted to when the topic event is fired
+      # @param secret [String] An optional shared secret used to generate a SHA1 HMAC of the outgoing body content
       # @return [Boolean] true if the subscribe was successful, otherwise an error is raised
       # @example Subscribe to push events from one of your repositories, having an email sent when fired
       #   client = Octokit::Client.new(:oauth_token = "token")
       #   client.subscribe("https://github.com/joshk/devise_imapable/events/push", "github://Email?address=josh.kalderimis@gmail.com")
-      def subscribe(topic, callback)
+      def subscribe(topic, callback, secret = nil)
         options = {
           :"hub.callback" => callback,
           :"hub.mode" => "subscribe",
           :"hub.topic" => topic
         }
+        options.merge!(:"hub.secret" => secret) unless secret.nil?
+
         response = pub_sub_hubbub_request(options)
 
         response.status == 204
@@ -58,13 +61,14 @@ module Octokit
       # @param service_arguments [Hash] params that will be passed by subscribed hook.
       #    List of services is available @ https://github.com/github/github-services/tree/master/docs.
       #    Please refer Data node for complete list of arguments.
+      # @param secret [String] An optional shared secret used to generate a SHA1 HMAC of the outgoing body content
       # @example Subscribe to push events to one of your repositories to Travis-CI
       #    client = Octokit::Client.new(:oauth_token = "token")
       #    client.subscribe_service_hook('joshk/device_imapable', 'Travis', { :token => "test", :domain => "domain", :user => "user" })
-      def subscribe_service_hook(repo, service_name, service_arguments = {})
+      def subscribe_service_hook(repo, service_name, service_arguments = {}, secret = nil)
         topic = "#{Octokit.web_endpoint}#{Repository.new(repo)}/events/push"
         callback = "github://#{service_name}?#{service_arguments.collect{ |k,v| [ k,v ].map{ |p| URI.encode_www_form_component(p) }.join("=") }.join("&") }"
-        subscribe(topic, callback)
+        subscribe(topic, callback, secret)
       end
 
       # Unsubscribe repository through pubsub
