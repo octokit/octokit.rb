a89cbfcabd541b7037127aa0ea08e604eeb6081f
Joey Wendt <joey@joeywendt.com>
2013-12-09 23:55:26 -0600

    Fix doc urls in Commits module
    [ci skip]

--- lib/octokit/client/commits.rb
+++ lib/octokit/client/commits.rb
@@ -19,7 +19,7 @@ module Octokit
       #   @param repo [String, Hash, Repository] A GitHub repository
       #   @param options [String] :sha Commit SHA or branch name from which to start the list
       # @return [Array<Sawyer::Resource>] An array of hashes representing commits
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
       def commits(*args)
         arguments = Octokit::RepoArguments.new(args)
         sha_or_branch = arguments.pop
@@ -43,7 +43,7 @@ module Octokit
       #   @param sha_or_branch [String] A commit SHA or branch name
       #   @param options [String] :sha Commit SHA or branch name from which to start the list
       # @return [Array<Sawyer::Resource>] An array of hashes representing commits
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
       # @example
       #   Octokit.commits_since('octokit/octokit.rb', '2012-10-01')
       def commits_since(*args)
@@ -70,7 +70,7 @@ module Octokit
       #   @param date [String] Date on which we want to compare
       #   @param sha_or_branch [String] Commit SHA or branch name from which to start the list
       # @return [Array<Sawyer::Resource>] An array of hashes representing commits
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
       # @example
       #   Octokit.commits_before('octokit/octokit.rb', '2012-10-01')
       def commits_before(*args)
@@ -97,7 +97,7 @@ module Octokit
       #   @param date [String] Date on which we want to compare
       #   @param sha_or_branch [String] Commit SHA or branch name from which to start the list
       # @return [Array<Sawyer::Resource>] An array of hashes representing commits
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
       # @example
       #   Octokit.commits_on('octokit/octokit.rb', '2012-10-01')
       def commits_on(*args)
@@ -126,7 +126,7 @@ module Octokit
       #   @param end_date [String] End Date on which we want to compare
       #   @param sha_or_branch [String] Commit SHA or branch name from which to start the list
       # @return [Array<Sawyer::Resource>] An array of hashes representing commits
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
       # @example
       #   Octokit.commits_on('octokit/octokit.rb', '2012-10-01', '2012-11-01')
       def commits_between(*args)
@@ -149,7 +149,7 @@ module Octokit
       # @param repo [String, Hash, Repository] A GitHub repository
       # @param sha [String] The SHA of the commit to fetch
       # @return [Sawyer::Resource] A hash representing the commit
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#get-a-single-commit
       def commit(repo, sha, options = {})
         get "repos/#{Repository.new(repo)}/commits/#{sha}", options
       end
@@ -166,7 +166,7 @@ module Octokit
       # @param tree [String] The SHA of the tree object the new commit will point to
       # @param parents [String, Array] One SHA (for a normal commit) or an array of SHAs (for a merge) of the new commit's parent commits. If ommitted or empty, a root commit will be created
       # @return [Sawyer::Resource] A hash representing the new commit
-      # @see http://developer.github.com/v3/git/commits/
+      # @see http://developer.github.com/v3/git/commits/#create-a-commit
       # @example Create a commit
       #   commit = Octokit.create_commit("octocat/Hello-World", "My commit message", "827efc6d56897b048c772eb4087f854f46256132", "7d1b31e74ee336d15cbd21741bc88a537ed063a0")
       #   commit.sha # => "7638417db6d59f3c431d3e1f261cc637155684cd"
@@ -183,7 +183,7 @@ module Octokit
       #
       # @param repo [String, Hash, Repository] A GitHub repository
       # @return [Array] An array of hashes representing comments
-      # @see http://developer.github.com/v3/repos/comments/
+      # @see http://developer.github.com/v3/repos/comments/#list-commit-comments-for-a-repository
       def list_commit_comments(repo, options = {})
         get "repos/#{Repository.new(repo)}/comments", options
       end
@@ -193,7 +193,7 @@ module Octokit
       # @param repo [String, Hash, Repository] A GitHub repository
       # @param sha [String] The SHA of the commit whose comments will be fetched
       # @return [Array] An array of hashes representing comments
-      # @see http://developer.github.com/v3/repos/comments/
+      # @see http://developer.github.com/v3/repos/comments/#list-comments-for-a-single-commit
       def commit_comments(repo, sha, options = {})
         get "repos/#{Repository.new(repo)}/commits/#{sha}/comments", options
       end
@@ -203,7 +203,7 @@ module Octokit
       # @param repo [String, Hash, Repository] A GitHub repository
       # @param id [String] The ID of the comment to fetch
       # @return [Sawyer::Resource] A hash representing the comment
-      # @see http://developer.github.com/v3/repos/comments/
+      # @see http://developer.github.com/v3/repos/comments/#get-a-single-commit-comment
       def commit_comment(repo, id, options = {})
         get "repos/#{Repository.new(repo)}/comments/#{id}", options
       end
@@ -217,7 +217,7 @@ module Octokit
       # @param line [Integer] Line number in the file to comment on
       # @param position [Integer] Line index in the diff to comment on
       # @return [Sawyer::Resource] A hash representing the new commit comment
-      # @see http://developer.github.com/v3/repos/comments/
+      # @see http://developer.github.com/v3/repos/comments/#create-a-commit-comment
       # @example Create a commit comment
       #   commit = Octokit.create_commit_comment("octocat/Hello-World", "827efc6d56897b048c772eb4087f854f46256132", "My comment message", "README.md", 10, 1)
       #   commit.commit_id # => "827efc6d56897b048c772eb4087f854f46256132"
@@ -242,7 +242,7 @@ module Octokit
       # @param id [String] The ID of the comment to update
       # @param body [String] Message
       # @return [Sawyer::Resource] A hash representing the updated commit comment
-      # @see http://developer.github.com/v3/repos/comments/
+      # @see http://developer.github.com/v3/repos/comments/#update-a-commit-comment
       # @example Update a commit comment
       #   commit = Octokit.update_commit_comment("octocat/Hello-World", "860296", "Updated commit comment")
       #   commit.id # => 860296
@@ -259,7 +259,7 @@ module Octokit
       # @param repo [String, Hash, Repository] A GitHub repository
       # @param id [String] The ID of the comment to delete
       # @return [nil] nil
-      # @see http://developer.github.com/v3/repos/comments/
+      # @see http://developer.github.com/v3/repos/comments/#delete-a-commit-comment
       def delete_commit_comment(repo, id, options = {})
         boolean_from_response :delete, "repos/#{Repository.new(repo)}/comments/#{id}", options
       end
@@ -270,7 +270,7 @@ module Octokit
       # @param start [String] The sha of the starting commit
       # @param endd [String] The sha of the ending commit
       # @return [Sawyer::Resource] A hash representing the comparison
-      # @see http://developer.github.com/v3/repos/commits/
+      # @see http://developer.github.com/v3/repos/commits/#compare-two-commits
       def compare(repo, start, endd, options = {})
         get "repos/#{Repository.new(repo)}/compare/#{start}...#{endd}", options
       end
@@ -282,7 +282,7 @@ module Octokit
       # @param head [String] The branch or SHA1 to merge
       # @option options [String] :commit_message The commit message for the merge
       # @return [Sawyer::Resource] A hash representing the comparison
-      # @see http://developer.github.com/v3/repos/merging/
+      # @see http://developer.github.com/v3/repos/merging/#perform-a-merge
       def merge(repo, base, head, options = {})
         params = {
           :base => base,
