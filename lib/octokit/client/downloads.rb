fc0850bee4c6b034e9aa96296b4a5d9cb06d5bb5
Joey Wendt <joey@joeywendt.com>
2014-01-09 09:55:58 -0600

    Remove deprecated method #create_download.

--- lib/octokit/client/downloads.rb
+++ lib/octokit/client/downloads.rb
@@ -32,44 +32,6 @@ module Octokit
         get "repos/#{Repository.new(repo)}/downloads/#{id}", options
       end
 
-      # Create a download in a repository
-      #
-      # @param repo [String, Repository, Hash] A GitHub repository
-      # @param name [String, Repository, Hash] A display name for the download
-      # @option options [String] :description The download description
-      # @option options [String] :content_type The content type. Defaults to 'text/plain'
-      # @return [Sawyer::Resource] A single download from the repository
-      # @deprecated As of December 11th, 2012: https://github.com/blog/1302-goodbye-uploads
-      # @see http://developer.github.com/v3/repos/downloads/#create-a-new-download-part-1-create-the-resource
-      # @example Create the "Robawt" download on Github/Hubot
-      #   Octokit.create_download("github/hubot", 'Robawt')
-      def create_download(repo, name, options={})
-        options[:content_type] ||= 'text/plain'
-        file = Faraday::UploadIO.new(name, options[:content_type])
-        resource = create_download_resource(repo, file.original_filename, File.size(name), options)
-
-        resource_hash = {
-          'key' => resource.path,
-          'acl' => resource.acl,
-          'success_action_status' => 201,
-          'Filename' => resource.name,
-          'AWSAccessKeyId' => resource.accesskeyid,
-          'Policy' => resource.policy,
-          'Signature' => resource.signature,
-          'Content-Type' => resource.mime_type,
-          'file' => file
-        }
-
-        conn = Faraday.new(resource.rels[:s3].href) do |builder|
-          builder.request :multipart
-          builder.request :url_encoded
-          builder.adapter :net_http
-        end
-
-        response = conn.post '/', resource_hash
-        response.status == 201
-      end
-
       # Delete a single download for a repository
       #
       # @param repo [String, Repository, Hash] A GitHub repository
@@ -83,11 +45,6 @@ module Octokit
         boolean_from_response :delete, "repos/#{Repository.new(repo)}/downloads/#{id}", options
       end
 
-      private
-
-      def create_download_resource(repo, name, size, options={})
-        post "repos/#{Repository.new(repo)}/downloads", options.merge({:name => name, :size => size})
-      end
-    end
+   end
   end
 end
--- spec/octokit/client/downloads_spec.rb
+++ spec/octokit/client/downloads_spec.rb
@@ -23,37 +23,12 @@ describe Octokit::Client::Downloads do
     end
   end # .download
 
-  context "methods that require a download" do
-
-    before(:each) do
-      local_path = File.expand_path("spec/fixtures/web_flow_token.json")
-      options = { :description => Time.now.to_i.to_s, :content_type => "text/plain" }
-      @client.create_download "api-playground/api-sandbox", local_path, options
-
-      @download = @client.downloads("api-playground/api-sandbox").last
+  describe ".delete_download", :vcr do
+    it "deletes a download" do
+      request = stub_delete(github_url("/repos/api-playground/api-sandbox/downloads/12345"))
+      @client.delete_download 'api-playground/api-sandbox', '12345'
+      assert_requested request
     end
-
-    after(:each) do
-      @client.delete_download 'api-playground/api-sandbox', @download.id
-    end
-
-    describe ".create_download", :vcr do
-      it "creates a download resource" do
-        assert_requested :post, github_url("/repos/api-playground/api-sandbox/downloads")
-      end
-      it "posts to an S3 url", :vcr do
-        assert_requested :post, "https://github.s3.amazonaws.com/"
-      end
-    end # .create_download
-
-
-    describe ".delete_download", :vcr do
-      it "deletes a download" do
-        @client.delete_download 'api-playground/api-sandbox', @download.id
-        assert_requested :delete, github_url("/repos/api-playground/api-sandbox/downloads/#{@download.id}")
-      end
-    end # .delete_download
-
-  end
+  end # .delete_download
 
 end
