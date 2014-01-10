e6f70d25abd81d1bfd6569a5a396dc03e5e38abc
Joey Wendt <joey@joeywendt.com>
2014-01-10 08:34:16 -0600

    Isolate Labels specs
    Moved .labels_for_milestone spec to top of file for context nesting
    clarity.
    
    Added "with repository" context with test repository setup and teardown.
    Moved specs referencing api-playground/api-sandbox into the context and
    updated relevant specs to use the repository setup in the context.
    
    Renamed "methods requiring a new label" context to "with label".
    
    Renamed "methods requiring a new issue" context to "with issue".

--- spec/octokit/client/labels_spec.rb
+++ spec/octokit/client/labels_spec.rb
@@ -1,7 +1,6 @@
 require 'helper'
 
 describe Octokit::Client::Labels do
-
   before do
     Octokit.reset!
     @client = oauth_client
@@ -23,79 +22,7 @@ describe Octokit::Client::Labels do
     end
   end # .label
 
-  describe ".add_label", :vcr do
-    it "adds a label with a color" do
-      @client.delete_label!("api-playground/api-sandbox", 'test-label', {:color => 'ededed'})
-      label = @client.add_label("api-playground/api-sandbox", "test-label", 'ededed')
-      expect(label.color).to eq "ededed"
-      assert_requested :post, github_url("/repos/api-playground/api-sandbox/labels")
-    end
-    it "adds a label with default color" do
-      @client.delete_label!("api-playground/api-sandbox", 'test-label', {:color => 'ededed'})
-      label = @client.add_label("api-playground/api-sandbox", "test-label")
-      assert_requested :post, github_url("/repos/api-playground/api-sandbox/labels")
-    end
-  end # .add_label
-
-  context "methods requiring a new label", :vcr do
-
-    before do
-      @client.delete_label!("api-playground/api-sandbox", 'test-label', {:color => 'ededed'})
-      @label = @client.add_label("api-playground/api-sandbox", "test-label", 'ededed')
-    end
-
-    describe ".update_label", :vcr do
-      it "updates a label with a new color" do
-        @client.update_label("api-playground/api-sandbox", @label.name, {:color => 'ffdd33'})
-        assert_requested :patch, github_url("/repos/api-playground/api-sandbox/labels/#{@label.name}")
-      end
-    end # .update_label
-  end
-
-  context "methods requiring a new issue", :vcr do
-    before do
-      @issue = @client.create_issue("api-playground/api-sandbox", "Issue for label test", "The body")
-    end
-
-    describe ".add_labels_to_an_issue", :vcr do
-      it "adds labels to a given issue" do
-        labels = @client.add_labels_to_an_issue('api-playground/api-sandbox', @issue.number, ['bug'])
-        assert_requested :post, github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels")
-      end
-    end # .add_labels_to_an_issue
-
-    describe ".labels_for_issue", :vcr do
-      it "returns all labels for a given issue" do
-        labels = @client.labels_for_issue('api-playground/api-sandbox', @issue.number)
-        expect(labels).to be_kind_of Array
-        assert_requested :get, github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}/labels")
-      end
-    end # .labels_for_issue
-
-    describe ".remove_label", :vcr do
-      it "removes a label from the specified issue" do
-        labels = @client.remove_label('api-playground/api-sandbox', @issue.number, 'bug')
-        assert_requested :delete, github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels/bug")
-      end
-    end # .remove_label
-
-    describe ".remove_all_labels", :vcr do
-      it "removes all labels from the specified issue" do
-        labels = @client.remove_all_labels('api-playground/api-sandbox', @issue.number)
-        assert_requested :delete, github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels")
-      end
-    end # .remove_all_labels
-
-    describe ".replace_all_labels", :vcr do
-      it "replaces all labels for an issue" do
-        labels = @client.replace_all_labels('api-playground/api-sandbox', @issue.number, ['bug', 'pdi'])
-        assert_requested :put, github_url("/repos/api-playground/api-sandbox/issues/#{@issue.number}" + "/labels")
-      end
-    end # .replace_all_labels
-  end
-
-
-  describe ".lables_for_milestone", :vcr do
+  describe ".labels_for_milestone", :vcr do
     it "returns all labels for a repository" do
       labels = @client.labels_for_milestone('octokit/octokit.rb', 2)
       expect(labels).to be_kind_of Array
@@ -103,13 +30,88 @@ describe Octokit::Client::Labels do
     end
   end # .labels_for_milestone
 
-
-  describe ".delete_label!", :vcr do
-    it "deletes a label from the repository" do
-      label = @client.add_label("api-playground/api-sandbox", "delete-me-label")
-      @client.delete_label!("api-playground/api-sandbox", label.name, {:color => 'ededed'})
-      assert_requested :delete, github_url("/repos/api-playground/api-sandbox/labels/#{label.name}")
+  context "with repository" do
+    before do
+      @test_repo = setup_test_repo.full_name
     end
-  end # .delete_label!
 
-end
+    after do
+      teardown_test_repo @test_repo
+    end
+
+    describe ".add_label", :vcr do
+      it "adds a label with a color" do
+        label = @client.add_label(@test_repo, "test-label", 'ededed')
+        expect(label.color).to eq "ededed"
+        assert_requested :post, github_url("/repos/#{@test_repo}/labels")
+      end
+
+      it "adds a label with default color" do
+        label = @client.add_label(@test_repo, "test-label")
+        assert_requested :post, github_url("/repos/#{@test_repo}/labels")
+      end
+    end # .add_label
+
+    context "with label", :vcr do
+      before do
+        @label = @client.add_label(@test_repo, "test-label", 'ededed')
+      end
+
+      describe ".update_label", :vcr do
+        it "updates a label with a new color" do
+          @client.update_label(@test_repo, @label.name, {:color => 'ffdd33'})
+          assert_requested :patch, github_url("/repos/#{@test_repo}/labels/#{@label.name}")
+        end
+      end # .update_label
+
+      describe ".delete_label!", :vcr do
+        it "deletes a label from the repository" do
+          @client.delete_label!(@test_repo, @label.name)
+          assert_requested :delete, github_url("/repos/#{@test_repo}/labels/#{@label.name}")
+        end
+      end # .delete_label!
+    end # with label
+
+    context "with issue", :vcr do
+      before do
+        @issue = @client.create_issue(@test_repo, "Issue for label test", "The body")
+      end
+
+      describe ".add_labels_to_an_issue", :vcr do
+        it "adds labels to a given issue" do
+          labels = @client.add_labels_to_an_issue(@test_repo, @issue.number, ['bug'])
+          assert_requested :post, github_url("/repos/#{@test_repo}/issues/#{@issue.number}/labels")
+        end
+      end # .add_labels_to_an_issue
+
+      describe ".labels_for_issue", :vcr do
+        it "returns all labels for a given issue" do
+          labels = @client.labels_for_issue(@test_repo, @issue.number)
+          expect(labels).to be_kind_of Array
+          assert_requested :get, github_url("/repos/#{@test_repo}/issues/#{@issue.number}/labels")
+        end
+      end # .labels_for_issue
+
+      describe ".remove_label", :vcr do
+        it "removes a label from the specified issue" do
+          labels = @client.remove_label(@test_repo, @issue.number, 'bug')
+          assert_requested :delete, github_url("/repos/#{@test_repo}/issues/#{@issue.number}/labels/bug")
+        end
+      end # .remove_label
+
+      describe ".remove_all_labels", :vcr do
+        it "removes all labels from the specified issue" do
+          labels = @client.remove_all_labels(@test_repo, @issue.number)
+          assert_requested :delete, github_url("/repos/#{@test_repo}/issues/#{@issue.number}/labels")
+        end
+      end # .remove_all_labels
+
+      describe ".replace_all_labels", :vcr do
+        it "replaces all labels for an issue" do
+          labels = @client.replace_all_labels(@test_repo, @issue.number, ['bug', 'pdi'])
+          assert_requested :put, github_url("/repos/#{@test_repo}/issues/#{@issue.number}/labels")
+        end
+      end # .replace_all_labels
+    end # with issue
+  end # with repository
+end # Octokit::Client::Labels
