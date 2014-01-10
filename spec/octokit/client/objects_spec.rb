2eb26b37346942963797523bbad4f1e1a6ed40e4
Joey Wendt <joey@joeywendt.com>
2014-01-10 07:22:31 -0600

    Isolate Objects specs
    Added "with repository" context with test repository setup and teardown.
    Moved specs referencing api-playground/api-sandbox into the context and
    updated the references to use the repository created in the context
    setup.

--- spec/octokit/client/objects_spec.rb
+++ spec/octokit/client/objects_spec.rb
@@ -1,7 +1,6 @@
 require 'helper'
 
 describe Octokit::Client::Objects do
-
   before do
     Octokit.reset!
     @client = oauth_client
@@ -22,13 +21,6 @@ describe Octokit::Client::Objects do
     end
   end # .tree
 
-  describe ".create_tree", :vcr do
-    it "creates a tree" do
-      tree = @client.create_tree("api-playground/api-sandbox", [ { "path" => "wynning.rb", "mode" => "100644", "type" => "blob", :content => "require 'fun'"} ])
-      assert_requested :post, github_url("/repos/api-playground/api-sandbox/git/trees")
-    end
-  end # .create_tree
-
   describe ".blob", :vcr do
     it "returns a blob" do
       blob = @client.blob("sferik/rails_admin", "94616fa57520ac8147522c7cf9f03d555595c5ea")
@@ -37,13 +29,6 @@ describe Octokit::Client::Objects do
     end
   end # .blob
 
-  describe ".create_blob", :vcr do
-    it "creates a blob" do
-      blob = @client.create_blob("api-playground/api-sandbox", "content")
-      assert_requested :post, github_url("/repos/api-playground/api-sandbox/git/blobs")
-    end
-  end # .create_blob
-
   describe ".tag", :vcr do
     it "returns a tag" do
       tag = @client.tag("octokit/octokit.rb", "23aad20633f4d2981b1c7209a800db3014774e96")
@@ -51,21 +36,45 @@ describe Octokit::Client::Objects do
     end
   end # .tag
 
-  describe ".create_tag", :vcr do
-    it "creates a tag" do
-      tag = @client.create_tag(
-        "api-playground/api-sandbox",
-        "v9000.0.0",
-        "Version 9000\n",
-        "eb11b3141c9dec3ba88d15b499d597a65df15320",
-        "commit",
-        "Wynn Netherland",
-        "wynn.netherland@gmail.com",
-        "2012-06-03T17:03:11-07:00"
-      )
-      expect(tag.tag).to eq "v9000.0.0"
-      assert_requested :post, github_url("/repos/api-playground/api-sandbox/git/tags")
+  context "with test repository" do
+    before do
+      @test_repo = setup_test_repo(:auto_init => true).full_name
     end
-  end # .create_tag
 
-end
+    after do
+      teardown_test_repo @test_repo
+    end
+
+    describe ".create_tree", :vcr do
+      it "creates a tree" do
+        tree = @client.create_tree(@test_repo, [ { "path" => "wynning.rb", "mode" => "100644", "type" => "blob", :content => "require 'fun'"} ])
+        assert_requested :post, github_url("/repos/#{@test_repo}/git/trees")
+      end
+    end # .create_tree
+
+    describe ".create_blob", :vcr do
+      it "creates a blob" do
+        blob = @client.create_blob(@test_repo, "content")
+        assert_requested :post, github_url("/repos/#{@test_repo}/git/blobs")
+      end
+    end # .create_blob
+
+    describe ".create_tag", :vcr do
+      it "creates a tag" do
+        sha = @client.commits(@test_repo).first.sha
+        tag = @client.create_tag(
+          @test_repo,
+          "v9000.0.0",
+          "Version 9000\n",
+          sha,
+          "commit",
+          "Wynn Netherland",
+          "wynn.netherland@gmail.com",
+          "2012-06-03T17:03:11-07:00"
+        )
+        expect(tag.tag).to eq "v9000.0.0"
+        assert_requested :post, github_url("/repos/#{@test_repo}/git/tags")
+      end
+    end # .create_tag
+  end # with test repository
+end # Octokit::Client::Objects
