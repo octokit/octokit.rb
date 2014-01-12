fe09cc5df949974d35030fb9354027e730d8953b
Joey Wendt <joey@joeywendt.com>
2014-01-12 15:22:05 -0600

    Isolate Contents specs
    Added "with repository" context with test repository setup and teardown.
    Moved specs referencing api-playground/api-sandbox into the context and
    updated specs to use repository setup in the context.

--- spec/octokit/client/contents_spec.rb
+++ spec/octokit/client/contents_spec.rb
@@ -1,7 +1,6 @@
 require 'helper'
 
 describe Octokit::Client::Contents do
-
   before do
     Octokit.reset!
     @client = oauth_client
@@ -33,99 +32,106 @@ describe Octokit::Client::Contents do
     end
   end # .archive_link
 
-  # TODO: Make the following specs idempotent
-
-  describe ".create_contents", :vcr do
-    it "creates repository contents at a path", :vcr do
-      response = @client.create_contents("api-playground/api-sandbox",
-                                         "test_create.txt",
-                                         "I am commit-ing",
-                                         "Here be the content\n")
-      expect(response.commit.sha).to match /[a-z0-9]{40}/
-      assert_requested(:put, github_url("/repos/api-playground/api-sandbox/contents/test_create.txt"))
+  context "with repository" do
+    before do
+      @test_repo = setup_test_repo.full_name
     end
-    it "creates contents from file path", :vcr do
-      response = @client.create_contents("api-playground/api-sandbox",
-                                         "test_create_path.txt",
-                                         "I am commit-ing",
-                                         :file => "spec/fixtures/new_file.txt")
-      expect(response.commit.sha).to match /[a-z0-9]{40}/
-      assert_requested(:put, github_url("/repos/api-playground/api-sandbox/contents/test_create_path.txt"))
-    end
-    it "creates contents from File object", :vcr do
-      file = File.new "spec/fixtures/new_file.txt", "r"
-      response = @client.create_contents("api-playground/api-sandbox",
-                                         "test_create_file.txt",
-                                         "I am commit-ing",
-                                         :file => file)
-      expect(response.commit.sha).to match /[a-z0-9]{40}/
-      assert_requested(:put, github_url("/repos/api-playground/api-sandbox/contents/test_create_file.txt"))
-    end
-    it "does not add new lines", :vcr do
-      file = File.new "spec/fixtures/large_file.txt", "r"
-      response = @client.create_contents("api-playground/api-sandbox",
-                                         "test_create_without_newlines.txt",
-                                         "I am commit-ing",
-                                         :file => file)
-      assert_requested(:put, github_url("/repos/api-playground/api-sandbox/contents/test_create_without_newlines.txt"))
-      content = response.content.rels[:self].get \
-        :headers => {:accept => "application/vnd.github.raw" }
-      expect(content.data).to eq(File.read("spec/fixtures/large_file.txt"))
-    end
-  end # .create_contents
 
-  describe ".update_contents", :vcr do
-    it "updates repository contents at a path" do
-      content = @client.create_contents("api-playground/api-sandbox",
-                                         "test_update.txt",
-                                         "I am commit-ing",
-                                         :file => "spec/fixtures/new_file.txt")
-      response = @client.update_contents("api-playground/api-sandbox",
-                                         "test_update.txt",
-                                         "I am commit-ing",
-                                         content.content.sha,
-                                         "Here be moar content")
-      expect(response.commit.sha).to match /[a-z0-9]{40}/
-      assert_requested :put,
-        github_url("/repos/api-playground/api-sandbox/contents/test_update.txt"),
-        :times => 2
-
+    after do
+      teardown_test_repo @test_repo
     end
-    it "does not add new lines", :vcr do
-      content = @client.create_contents("api-playground/api-sandbox",
-                                         "test_update_without_newlines.txt",
-                                         "I am commit-ing",
-                                         :file => "spec/fixtures/new_file.txt")
-      response = @client.update_contents("api-playground/api-sandbox",
-                                         "test_update_without_newlines.txt",
-                                         "I am commit-ing",
-                                         content.content.sha,
-                                         :file => "spec/fixtures/large_file.txt")
 
-      assert_requested :put,
-        github_url("/repos/api-playground/api-sandbox/contents/test_update_without_newlines.txt"),
-        :times => 2
+    describe ".create_contents", :vcr do
+      it "creates repository contents at a path", :vcr do
+        response = @client.create_contents(@test_repo,
+                                           "test_create.txt",
+                                           "I am commit-ing",
+                                           "Here be the content\n")
+        expect(response.commit.sha).to match /[a-z0-9]{40}/
+        assert_requested(:put, github_url("/repos/#{@test_repo}/contents/test_create.txt"))
+      end
+      it "creates contents from file path", :vcr do
+        response = @client.create_contents(@test_repo,
+                                           "test_create_path.txt",
+                                           "I am commit-ing",
+                                           :file => "spec/fixtures/new_file.txt")
+        expect(response.commit.sha).to match /[a-z0-9]{40}/
+        assert_requested(:put, github_url("/repos/#{@test_repo}/contents/test_create_path.txt"))
+      end
+      it "creates contents from File object", :vcr do
+        file = File.new "spec/fixtures/new_file.txt", "r"
+        response = @client.create_contents(@test_repo,
+                                           "test_create_file.txt",
+                                           "I am commit-ing",
+                                           :file => file)
+        expect(response.commit.sha).to match /[a-z0-9]{40}/
+        assert_requested(:put, github_url("/repos/#{@test_repo}/contents/test_create_file.txt"))
+      end
+      it "does not add new lines", :vcr do
+        file = File.new "spec/fixtures/large_file.txt", "r"
+        response = @client.create_contents(@test_repo,
+                                           "test_create_without_newlines.txt",
+                                           "I am commit-ing",
+                                           :file => file)
+        assert_requested(:put, github_url("/repos/#{@test_repo}/contents/test_create_without_newlines.txt"))
+        content = response.content.rels[:self].get \
+          :headers => {:accept => "application/vnd.github.raw" }
+        expect(content.data).to eq(File.read("spec/fixtures/large_file.txt"))
+      end
+    end # .create_contents
 
-      content = response.content.rels[:self].get \
-        :headers => {:accept => "application/vnd.github.raw" }
-      expect(content.data).to eq(File.read("spec/fixtures/large_file.txt"))
-    end
-  end # .update_contents
+    describe ".update_contents", :vcr do
+      it "updates repository contents at a path" do
+        content = @client.create_contents(@test_repo,
+                                           "test_update.txt",
+                                           "I am commit-ing",
+                                           :file => "spec/fixtures/new_file.txt")
+        response = @client.update_contents(@test_repo,
+                                           "test_update.txt",
+                                           "I am commit-ing",
+                                           content.content.sha,
+                                           "Here be moar content")
+        expect(response.commit.sha).to match /[a-z0-9]{40}/
+        assert_requested :put,
+          github_url("/repos/#{@test_repo}/contents/test_update.txt"),
+          :times => 2
 
-  describe ".delete_contents", :vcr do
-    it "deletes repository contents at a path" do
-      content = @client.create_contents("api-playground/api-sandbox",
-                                         "test_delete.txt",
-                                         "I am commit-ing",
-                                         "You DELETE me")
-      response = @client.delete_contents("api-playground/api-sandbox",
-                                         "test_delete.txt",
-                                         "I am rm-ing",
-                                         content.content.sha)
-      expect(response.commit.sha).to match /[a-z0-9]{40}/
-      assert_requested :delete,
-        github_url("/repos/api-playground/api-sandbox/contents/test_delete.txt")
-    end
-  end # .delete_contents
+      end
+      it "does not add new lines", :vcr do
+        content = @client.create_contents(@test_repo,
+                                           "test_update_without_newlines.txt",
+                                           "I am commit-ing",
+                                           :file => "spec/fixtures/new_file.txt")
+        response = @client.update_contents(@test_repo,
+                                           "test_update_without_newlines.txt",
+                                           "I am commit-ing",
+                                           content.content.sha,
+                                           :file => "spec/fixtures/large_file.txt")
 
-end
+        assert_requested :put,
+          github_url("/repos/#{@test_repo}/contents/test_update_without_newlines.txt"),
+          :times => 2
+
+        content = response.content.rels[:self].get \
+          :headers => {:accept => "application/vnd.github.raw" }
+        expect(content.data).to eq(File.read("spec/fixtures/large_file.txt"))
+      end
+    end # .update_contents
+
+    describe ".delete_contents", :vcr do
+      it "deletes repository contents at a path" do
+        content = @client.create_contents(@test_repo,
+                                           "test_delete.txt",
+                                           "I am commit-ing",
+                                           "You DELETE me")
+        response = @client.delete_contents(@test_repo,
+                                           "test_delete.txt",
+                                           "I am rm-ing",
+                                           content.content.sha)
+        expect(response.commit.sha).to match /[a-z0-9]{40}/
+        assert_requested :delete,
+          github_url("/repos/#{@test_repo}/contents/test_delete.txt")
+      end
+    end # .delete_contents
+  end # with repository
+end # Octokit::Client::Contents
