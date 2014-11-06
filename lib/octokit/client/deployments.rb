e1ddb9d49233f6a28f9a1efa5b0e1df80805f756
Blake Gentry <blakesgentry@gmail.com>
2014-11-05 20:38:28 -0800

    document the task option

--- lib/octokit/client/deployments.rb
+++ lib/octokit/client/deployments.rb
@@ -23,6 +23,7 @@ module Octokit
       #
       # @param repo [Integer, String, Repository, Hash] A GitHub repository
       # @param ref [String] The ref to deploy
+      # @option options [String] :task Used by the deployment system to allow different execution paths. Defaults to "deploy".
       # @option options [String] :payload Meta info about the deployment
       # @option options [Boolean] :auto_merge Optional parameter to merge the default branch into the requested deployment branch if necessary. Default: true
       # @option options [String] :description Optional short description.
