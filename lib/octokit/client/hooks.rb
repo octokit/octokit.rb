526afa9633ce8eda7cd0a5340f4072e05ad41813
Wynn Netherland <wynn@github.com>
2014-12-03 10:31:31 -0600

    Move repo hooks methods into Hooks module

--- lib/octokit/client/hooks.rb
+++ lib/octokit/client/hooks.rb
@@ -13,6 +13,141 @@ module Octokit
       def available_hooks(options = {})
         get "hooks", options
       end
+
+      # List repo hooks
+      #
+      # Requires authenticated client.
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository.
+      # @return [Array<Sawyer::Resource>] Array of hashes representing hooks.
+      # @see https://developer.github.com/v3/repos/hooks/#list-hooks
+      # @example
+      #   @client.hooks('octokit/octokit.rb')
+      def hooks(repo, options = {})
+        paginate "#{Repository.path repo}/hooks", options
+      end
+
+      # Get single hook
+      #
+      # Requires authenticated client.
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository.
+      # @param id [Integer] Id of the hook to get.
+      # @return [Sawyer::Resource] Hash representing hook.
+      # @see https://developer.github.com/v3/repos/hooks/#get-single-hook
+      # @example
+      #   @client.hook('octokit/octokit.rb', 100000)
+      def hook(repo, id, options = {})
+        get "#{Repository.path repo}/hooks/#{id}", options
+      end
+
+      # Create a hook
+      #
+      # Requires authenticated client.
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository.
+      # @param name [String] The name of the service that is being called. See
+      #   {https://api.github.com/hooks Hooks} for the possible names.
+      # @param config [Hash] A Hash containing key/value pairs to provide
+      #   settings for this hook. These settings vary between the services and
+      #   are defined in the {https://github.com/github/github-services github-services} repo.
+      # @option options [Array<String>] :events ('["push"]') Determines what
+      #   events the hook is triggered for.
+      # @option options [Boolean] :active Determines whether the hook is
+      #   actually triggered on pushes.
+      # @return [Sawyer::Resource] Hook info for the new hook
+      # @see https://api.github.com/hooks
+      # @see https://github.com/github/github-services
+      # @see https://developer.github.com/v3/repos/hooks/#create-a-hook
+      # @example
+      #   @client.create_hook(
+      #     'octokit/octokit.rb',
+      #     'web',
+      #     {
+      #       :url => 'http://something.com/webhook',
+      #       :content_type => 'json'
+      #     },
+      #     {
+      #       :events => ['push', 'pull_request'],
+      #       :active => true
+      #     }
+      #   )
+      def create_hook(repo, name, config, options = {})
+        options = {:name => name, :config => config, :events => ["push"], :active => true}.merge(options)
+        post "#{Repository.path repo}/hooks", options
+      end
+
+      # Edit a hook
+      #
+      # Requires authenticated client.
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository.
+      # @param id [Integer] Id of the hook being updated.
+      # @param name [String] The name of the service that is being called. See
+      #   {https://api.github.com/hooks Hooks} for the possible names.
+      # @param config [Hash] A Hash containing key/value pairs to provide
+      #   settings for this hook. These settings vary between the services and
+      #   are defined in the {https://github.com/github/github-services github-services} repo.
+      # @option options [Array<String>] :events ('["push"]') Determines what
+      #   events the hook is triggered for.
+      # @option options [Array<String>] :add_events Determines a list of events
+      #   to be added to the list of events that the Hook triggers for.
+      # @option options [Array<String>] :remove_events Determines a list of events
+      #   to be removed from the list of events that the Hook triggers for.
+      # @option options [Boolean] :active Determines whether the hook is
+      #   actually triggered on pushes.
+      # @return [Sawyer::Resource] Hook info for the updated hook
+      # @see https://api.github.com/hooks
+      # @see https://github.com/github/github-services
+      # @see https://developer.github.com/v3/repos/hooks/#edit-a-hook
+      # @example
+      #   @client.edit_hook(
+      #     'octokit/octokit.rb',
+      #     100000,
+      #     'web',
+      #     {
+      #       :url => 'http://something.com/webhook',
+      #       :content_type => 'json'
+      #     },
+      #     {
+      #       :add_events => ['status'],
+      #       :remove_events => ['pull_request'],
+      #       :active => true
+      #     }
+      #   )
+      def edit_hook(repo, id, name, config, options = {})
+        options = {:name => name, :config => config, :events => ["push"], :active => true}.merge(options)
+        patch "#{Repository.path repo}/hooks/#{id}", options
+      end
+
+      # Delete hook
+      #
+      # Requires authenticated client.
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository.
+      # @param id [Integer] Id of the hook to remove.
+      # @return [Boolean] True if hook removed, false otherwise.
+      # @see https://developer.github.com/v3/repos/hooks/#delete-a-hook
+      # @example
+      #   @client.remove_hook('octokit/octokit.rb', 1000000)
+      def remove_hook(repo, id, options = {})
+        boolean_from_response :delete, "#{Repository.path repo}/hooks/#{id}", options
+      end
+
+      # Test hook
+      #
+      # Requires authenticated client.
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository.
+      # @param id [Integer] Id of the hook to test.
+      # @return [Boolean] Success
+      # @see https://developer.github.com/v3/repos/hooks/#test-a-push-hook
+      # @example
+      #   @client.test_hook('octokit/octokit.rb', 1000000)
+      def test_hook(repo, id, options = {})
+        boolean_from_response :post, "#{Repository.path repo}/hooks/#{id}/tests", options
+      end
+
     end
   end
 end
--- spec/octokit/client/hooks_spec.rb
+++ spec/octokit/client/hooks_spec.rb
@@ -2,11 +2,81 @@ require 'helper'
 
 describe Octokit::Client::Hooks do
 
+  before do
+    Octokit.reset!
+    @client = oauth_client
+  end
+
   describe ".available_hooks", :vcr do
     it "returns all the hooks supported by GitHub with their parameters" do
-      client = oauth_client
-      hooks = client.available_hooks
+      hooks = @client.available_hooks
       expect(hooks.first.name).to eq("activecollab")
     end
   end
+
+  context "with repository" do
+    before(:each) do
+      @repo = @client.create_repository("an-repo")
+    end
+
+    after(:each) do
+      begin
+        @client.delete_repository(@repo.full_name)
+      rescue Octokit::NotFound
+      end
+    end
+
+    describe ".hooks", :vcr do
+      it "returns a repository's hooks" do
+        hooks = @client.hooks(@repo.full_name)
+        expect(hooks).to be_kind_of Array
+        assert_requested :get, github_url("/repos/#{@repo.full_name}/hooks")
+      end
+    end
+
+    context "with hook" do
+      before(:each) do
+        @hook = @client.create_hook(@repo.full_name, "railsbp", {:railsbp_url => "http://railsbp.com", :token => "xAAQZtJhYHGagsed1kYR"})
+      end
+
+      after(:each) do
+        @client.remove_hook(@repo.full_name, @hook.id)
+      end
+
+      describe ".create_hook", :vcr do
+        it "creates a hook" do
+          assert_requested :post, github_url("/repos/#{@repo.full_name}/hooks")
+        end
+      end # .create_hook
+
+      describe ".hook", :vcr do
+        it "returns a repository's single hook" do
+          @client.hook(@repo.full_name, @hook.id)
+          assert_requested :get, github_url("/repos/#{@repo.full_name}/hooks/#{@hook.id}")
+        end
+      end # .hook
+
+      describe ".edit_hook", :vcr do
+        it "edits a hook" do
+          @client.edit_hook(@repo.full_name, @hook.id, "railsbp", {:railsbp_url => "https://railsbp.com", :token => "xAAQZtJhYHGagsed1kYR"})
+          assert_requested :patch, github_url("/repos/#{@repo.full_name}/hooks/#{@hook.id}")
+        end
+      end # .edit_hook
+
+      describe ".test_hook", :vcr do
+        it "tests a hook" do
+          @client.create_hook(@repo.full_name, "railsbp", {:railsbp_url => "http://railsbp.com", :token => "xAAQZtJhYHGagsed1kYR"})
+          @client.test_hook(@repo.full_name, @hook.id)
+          assert_requested :post, github_url("/repos/#{@repo.full_name}/hooks/#{@hook.id}/tests")
+        end
+      end # .test_hook
+
+      describe ".remove_hook", :vcr do
+        it "removes a hook" do
+          @client.remove_hook(@repo.full_name, @hook.id)
+          assert_requested :delete, github_url("/repos/#{@repo.full_name}/hooks/#{@hook.id}")
+        end
+      end # .remove_hook
+    end # with hook
+  end # with repository
 end
