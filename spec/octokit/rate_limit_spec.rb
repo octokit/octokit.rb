ce9483b52fa36b4639b8c86cc2fc13bf17c5abea
Carsten Wirth <carsten.wirth@blacklane.com>
2014-12-07 21:35:20 +0100

    imporiving rate_limit struct spec

--- spec/octokit/rate_limit_spec.rb
+++ spec/octokit/rate_limit_spec.rb
@@ -2,57 +2,60 @@ require 'helper'
 require 'octokit/rate_limit'
 
 describe Octokit::RateLimit do
+  subject { described_class }
 
-  it "parses rate limit info from response headers" do
-    response = double()
-    expect(response).to receive(:headers).
-      at_least(:once).
-      and_return({
+  describe ".from_response" do
+    let(:resonse_headers) do
+      {
         "X-RateLimit-Limit" => 60,
         "X-RateLimit-Remaining" => 42,
         "X-RateLimit-Reset" => (Time.now + 60).to_i
-      })
-    info = Octokit::RateLimit.from_response(response)
-    expect(info.limit).to eq(60)
-    expect(info.remaining).to eq(42)
-    expect(info.resets_in).to eq(59)
-    expect(info.resets_at).to be_kind_of(Time)
-  end
+      }
+    end
 
-  it "returns a positive rate limit for Enterprise" do
-    response = double()
-    expect(response).to receive(:headers).
-      at_least(:once).
-      and_return({ })
-    info = Octokit::RateLimit.from_response(response)
-    expect(info.limit).to eq(1)
-    expect(info.remaining).to eq(1)
-    expect(info.resets_in).to eq(0)
-    expect(info.resets_at).to be_kind_of(Time)
-  end
+    let(:response) { double("response") }
 
-  it "handles nil responses" do
-    info = Octokit::RateLimit.from_response(nil)
-    expect(info.limit).to be_nil
-    expect(info.remaining).to be_nil
-    expect(info.resets_in).to be_nil
-    expect(info.resets_at).to be_nil
-  end
+    it "parses rate limit info from response headers" do
+      expect(response).to receive(:headers).
+        at_least(:once).
+        and_return(resonse_headers)
+      info = described_class.from_response(response)
+      expect(info.limit).to eq(60)
+      expect(info.remaining).to eq(42)
+      expect(info.resets_in).to eq(59)
+      expect(info.resets_at).to be_kind_of(Time)
+    end
 
-  it "handles resets_in time in past" do
-    response = double()
-    expect(response).to receive(:headers).
-      at_least(:once).
-      and_return({
-        "X-RateLimit-Limit" => 60,
-        "X-RateLimit-Remaining" => 42,
-        "X-RateLimit-Reset" => (Time.now - 60).to_i
-      })
-    info = Octokit::RateLimit.from_response(response)
-    expect(info.limit).to eq(60)
-    expect(info.remaining).to eq(42)
-    expect(info.resets_in).to eq(0)
-    expect(info.resets_at).to be_kind_of(Time)
-  end
+    it "returns a positive rate limit for Enterprise" do
+      expect(response).to receive(:headers).
+        at_least(:once).
+        and_return({ })
+      info = described_class.from_response(response)
+      expect(info.limit).to eq(1)
+      expect(info.remaining).to eq(1)
+      expect(info.resets_in).to eq(0)
+      expect(info.resets_at).to be_kind_of(Time)
+    end
 
+    it "handles nil responses" do
+      info = described_class.from_response(nil)
+      expect(info.limit).to be_nil
+      expect(info.remaining).to be_nil
+      expect(info.resets_in).to be_nil
+      expect(info.resets_at).to be_nil
+    end
+
+    it "handles resets_in time in past" do
+      expect(response).to receive(:headers).
+        at_least(:once).
+        and_return(
+          resonse_headers.merge("X-RateLimit-Reset" => (Time.now - 60).to_i)
+        )
+      info = described_class.from_response(response)
+      expect(info.limit).to eq(60)
+      expect(info.remaining).to eq(42)
+      expect(info.resets_in).to eq(0)
+      expect(info.resets_at).to be_kind_of(Time)
+    end
+  end
 end
