a274283baf1d8ae24f28c15fe8eb0d774fb2a446
Ben Balter <ben.balter@github.com>
2015-10-16 11:06:30 -0400

    add license_contents method

--- spec/octokit/client/licenses_spec.rb
+++ spec/octokit/client/licenses_spec.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 describe Octokit::Client::Licenses do
-	
+
   describe ".licenses", :vcr do
     it "returns all licenses" do
       licenses = Octokit.licenses :accept => "application/vnd.github.drax-preview+json"
@@ -15,4 +15,13 @@ describe Octokit::Client::Licenses do
       expect(license.name).to eq("MIT License")
     end
   end
+
+	descibe ".license_contents", :vcr do
+		it "returns a repository's license file" do
+			response = Octokit.license_contents 'benbalter/licensee', :accept => "application/vnd.github.drax-preview+json"
+			expect(response.license.key).to eql("mit")
+			content = Base64.decode64 response.license.content
+			expect(content).to match(/MIT/) 
+		end
+	end
 end # Octokit::Licenses
