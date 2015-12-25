6ca8ce92d713a07be9488f6e59c2754ea97d8fab
Wynn Netherland <wynn@github.com>
2015-12-25 12:32:24 -0600

    Separate options for retry timeout and retry wait

--- lib/octokit/client/stats.rb
+++ lib/octokit/client/stats.rb
@@ -9,6 +9,8 @@ module Octokit
       # Get contributors list with additions, deletions, and commit counts
       #
       # @param repo [Integer, String, Hash, Repository] A GitHub repository
+      # @option retry_timeout [Number] How long Octokit should keep trying to get stats (in seconds)
+      # @option retry_wait [Number] How long Octokit should wait between retries.
       # @return [Array<Sawyer::Resource>] Array of contributor stats
       # @see https://developer.github.com/v3/repos/statistics/#get-contributors-list-with-additions-deletions-and-commit-counts
       # @example Get contributor stats for octokit
@@ -21,6 +23,8 @@ module Octokit
       # Get the last year of commit activity data
       #
       # @param repo [Integer, String, Hash, Repository] A GitHub repository
+      # @option retry_timeout [Number] How long Octokit should keep trying to get stats (in seconds)
+      # @option retry_wait [Number] How long Octokit should wait between retries.
       # @return [Array<Sawyer::Resource>] The last year of commit activity grouped by
       #   week. The days array is a group of commits per day, starting on Sunday.
       # @see https://developer.github.com/v3/repos/statistics/#get-the-last-year-of-commit-activity-data
@@ -33,6 +37,8 @@ module Octokit
       # Get the number of additions and deletions per week
       #
       # @param repo [Integer, String, Hash, Repository] A GitHub repository
+      # @option retry_timeout [Number] How long Octokit should keep trying to get stats (in seconds)
+      # @option retry_wait [Number] How long Octokit should wait between retries.
       # @return [Array<Sawyer::Resource>] Weekly aggregate of the number of additions
       #   and deletions pushed to a repository.
       # @see https://developer.github.com/v3/repos/statistics/#get-the-number-of-additions-and-deletions-per-week
@@ -45,6 +51,8 @@ module Octokit
       # Get the weekly commit count for the repo owner and everyone else
       #
       # @param repo [Integer, String, Hash, Repository] A GitHub repository
+      # @option retry_timeout [Number] How long Octokit should keep trying to get stats (in seconds)
+      # @option retry_wait [Number] How long Octokit should wait between retries.
       # @return [Sawyer::Resource] Total commit counts for the owner and total commit
       #   counts in all. all is everyone combined, including the owner in the last
       #   52 weeks. If you’d like to get the commit counts for non-owners, you can
@@ -59,6 +67,8 @@ module Octokit
       # Get the number of commits per hour in each day
       #
       # @param repo [Integer, String, Hash, Repository] A GitHub repository
+      # @option retry_timeout [Number] How long Octokit should keep trying to get stats (in seconds)
+      # @option retry_wait [Number] How long Octokit should wait between retries.
       # @return [Array<Array>] Arrays containing the day number, hour number, and
       #   number of commits
       # @see https://developer.github.com/v3/repos/statistics/#get-the-number-of-commits-per-hour-in-each-day
@@ -80,7 +90,8 @@ module Octokit
       def get_stats(repo, metric, options = {})
         path = "#{Repository.path repo}/stats/#{metric}"
         if retry_timeout = options.delete(:retry_timeout)
-          get_stats_with_retry(retry_timeout, path, options)
+          retry_wait = options.delete(:retry_wait) || 0.5
+          get_stats_with_retry(path, retry_timeout, retry_wait, options)
         else
           get(path, options)
         end.tap do
@@ -90,16 +101,19 @@ module Octokit
 
       # @private Get stats for a repository. Retry call until stats are ready or retry_timeout exceeded.
       #
-      # @param retry_timeout [String] For how long Octokit should keep trying to get stats (in seconds)
+      # @param path [String] The path, relative to {#api_endpoint}
+      # @param timeout [Number] How long Octokit should keep trying to get stats (in seconds)
+      # @param wait [Number] How long Octokit should wait between retries.
+      # @param options [Hash] Query and header params for request
       # @return [Array<Sawyer::Resource> or nil] Stats in metric-specific format, or nil if not yet calculated.
-      def get_stats_with_retry(retry_timeout, path, options = {})
+      def get_stats_with_retry(path, retry_timeout, retry_wait, options = {})
         time_start = Time.now
 
         loop do
           data = get(path, options)
           timeout_exceeded = Time.now - time_start >= retry_timeout
           return data if last_response.status != 202 || timeout_exceeded
-          sleep retry_timeout / 4.0
+          sleep retry_wait
         end
       end
     end
