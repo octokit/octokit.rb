cc67e541bb4d73d9deb9fb83adef0c04152c189e
Wynn Netherland <wynn@github.com>
2016-03-04 20:44:54 -0600

    Stub Octokit::Response

---
+++ spec/octokit/response_spec.rb
@@ -0,0 +1,13 @@
+require 'helper'
+
+describe Octokit::Response do
+
+  describe "#status"
+  describe "#headers"
+  describe "#body"
+  describe "#data" do
+    it "handles arrays of strings"
+  end
+  describe "#rels"
+  describe "#time"
+end
