b3e20b7efe547eddd56e5a5f7621795f2b5d0821
Wynn Netherland <wynn@github.com>
2016-03-04 20:38:32 -0600

    Stub specs for Octokit::Resource

---
+++ spec/octokit/resource_spec.rb
@@ -0,0 +1,56 @@
+require 'helper'
+
+describe Octokit::Resource do
+
+  it "provides dot notation for keys"
+
+  it "handles string keys for hash notation"
+
+  it "handle clashing keys"
+
+  it "handles nested objects"
+
+  it "handles nested collections"
+
+  it "provides attribute predicates"
+
+  it "provides attribute setters"
+
+  it "provides dynamic attribute methods from getters"
+
+  describe "#attr" do
+    it "returns a hash"
+  end
+
+  describe "#to_h" do
+    it "returns a hash"
+
+    it "supports nesting"
+  end
+
+  describe "#to_attrs" do
+    it "returns a hash"
+  end
+
+  context "rel parsing" do
+
+    it "parses *_url fields as rels"
+
+  end
+
+  context "dumping" do
+
+    it "supports YAML dump"
+
+    it "supports Marshal.dump"
+
+  end
+
+  describe "#inspect"
+
+  describe "#each"
+
+  describe "#enumerable"
+
+end
+
