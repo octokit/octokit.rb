1e82afe8590227b25da540910ad75c1960131565
Olle Jonsson <olle.jonsson@gmail.com>
2016-07-02 14:28:47 +0200

    Repository: Avoid obsolete warning using URI::ABS_URI

--- lib/octokit/repository.rb
+++ lib/octokit/repository.rb
@@ -78,7 +78,7 @@ module Octokit
     private
 
       def validate_owner_and_name!
-        if @owner.include?('/') || @name.include?('/') || !url.match(/\A#{URI.regexp}\z/)
+        if @owner.include?('/') || @name.include?('/') || !url.match(/\A#{URI::ABS_URI}\z/)
           raise_invalid_repository!
         end
       end
