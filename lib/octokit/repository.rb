54fc6d2a7069770b2cb1da0da2bfbc6d9525c8c0
Joey Wendt <joey@joeywendt.com>
2016-10-22 17:22:21 -0500

    Remove \A and \z, URI::ABS_URL include them

--- lib/octokit/repository.rb
+++ lib/octokit/repository.rb
@@ -78,7 +78,7 @@ module Octokit
     private
 
       def validate_owner_and_name!
-        if @owner.include?('/') || @name.include?('/') || !url.match(/\A#{URI::ABS_URI}\z/)
+        if @owner.include?('/') || @name.include?('/') || !url.match(URI::ABS_URI)
           raise_invalid_repository!
         end
       end
