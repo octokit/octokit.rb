b46922b7ee056823150a1a9e27038a077ddffdb9
Joey Wendt <joey@joeywendt.com>
2016-10-23 11:22:17 -0500

    Add Projects.projects for List projects endpoint

--- lib/octokit/client/projects.rb
+++ lib/octokit/client/projects.rb
@@ -5,7 +5,21 @@ module Octokit
     #
     # @see https://developer.github.com/v3/repos/projects/#projects
     module Projects
-    end # Projects
 
+      # List projects for a repository
+      #
+      # Requires authenticated client
+      #
+      # @param repo [Integer, String, Repository, Hash] A GitHub repository
+      # @return [Array<Sawyer::Resource>] Repository projects
+      # @see https://developer.github.com/v3/repos/projects/#list-projects
+      # @example
+      #   @client.projects('octokit/octokit.rb')
+      def projects(repo, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        paginate "#{Repository.path repo}/projects", opts
+      end
+
+    end # Projects
   end
 end
--- spec/octokit/client/projects_spec.rb
+++ spec/octokit/client/projects_spec.rb
@@ -1,4 +1,13 @@
 require 'helper'
 
 describe Octokit::Client::Projects do
+
+  describe ".projects", :vcr do
+    it "returns a list of projects for a repository" do
+      projects = oauth_client.projects('octokit/octokit.rb')
+      expect(projects).to be_kind_of Array
+      assert_requested :get, github_url("/repos/octokit/octokit.rb/projects")
+    end
+  end # .projects
+
 end # Octokit::Client::Projects
