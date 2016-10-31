d14f5aadf401a64b31a26563e76740e644b54821
Joey Wendt <joey@joeywendt.com>
2016-10-30 23:10:43 -0500

    Add Projects Apis

--- lib/octokit/client/projects.rb
+++ lib/octokit/client/projects.rb
@@ -3,7 +3,7 @@ module Octokit
 
     # Methods for Projects API
     #
-    # @see https://developer.github.com/v3/repos/projects/#projects
+    # @see https://developer.github.com/v3/repos/projects
     module Projects
 
       # List projects for a repository
@@ -12,7 +12,7 @@ module Octokit
       #
       # @param repo [Integer, String, Repository, Hash] A GitHub repository
       # @return [Array<Sawyer::Resource>] Repository projects
-      # @see https://developer.github.com/v3/repos/projects/#list-projects
+      # @see https://developer.github.com/v3/projects/#list-repository-projects 
       # @example
       #   @client.projects('octokit/octokit.rb')
       def projects(repo, options = {})
@@ -28,7 +28,7 @@ module Octokit
       # @param name [String] Project name
       # @option options [String] :body Body of the project
       # @return [Sawyer::Resource] Fresh new project
-      # @see https://developer.github.com/v3/repos/projects/#create-a-project
+      # @see https://developer.github.com/v3/projects/#create-a-repository-project 
       # @example Create project with only a name
       #   @client.create_project('octokit/octokit.rb', 'implement new APIs')
       #
@@ -40,160 +40,273 @@ module Octokit
         post "#{Repository.path repo}/projects", opts
       end
 
-      # Get a project by number
+      # List organization projects
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub Repository
-      # @param number [Integer] The project number
+      # Requires authenticated client
+      #
+      # @param org [String] A GitHub organization
+      # @return [Array<Sawyer::Resource>] Organization projects
+      # @see https://developer.github.com/v3/projects/#list-organization-projects
+      # @example
+      #   @client.org_projects("octokit")
+      def org_projects(org, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        get "orgs/#{org}/projects", opts
+      end
+      alias :organization_projects :org_projects
+
+      # Create organization project
+      #
+      # Requires authenticated client
+      #
+      # @param org [String] A GitHub organization
+      # @param name [String] Project name
+      # @option options [String] :body Project body
+      # @return [Sawyer::Resource] Organization project
+      # @see https://developer.github.com/v3/projects/#create-an-organization-project
+      # @example Create with only a name
+      #   @client.create_org_project("octocat", "make more octocats")
+      # @example Create a project with name and body
+      #   @client.create_org_project("octokit", "octocan", body: 'Improve clients')
+      def create_org_project(org, name, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        opts[:name] = name
+        post "orgs/#{org}/projects", opts
+      end
+      alias :create_organization_project :create_org_project
+
+      # Get a project by id 
+      #
+      # @param id [Integer] Project id
       # @return [Sawyer::Resource] Project
-      # @see https://developer.github.com/v3/repos/projects/#get-a-project
+      # @see https://developer.github.com/v3/projects/#get-a-project 
       # @example
       #   Octokit.project("octokit/octokit.rb", 1)
-      def project(repo, number, options = {})
+      def project(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        get "#{Repository.path repo}/projects/#{number}", opts
+        get "projects/#{id}", opts
       end
 
       # Update a project
       #
       # Requires authenticated client
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
+      # @param id [Integer] Project id
       # @option options [String] :name Project name
       # @option options [String] :body Project body
       # @return [Sawyer::Resource] Project
-      # @see https://developer.github.com/v3/repos/projects/#update-a-project
+      # @see https://developer.github.com/v3/projects/#update-a-project 
       # @example Update project name
-      #   @client.update_project("octokit/octokit.rb", 1, name: 'New name')
-      def update_project(repo, number, options = {})
+      #   @client.update_project(123942, name: 'New name')
+      def update_project(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        patch "#{Repository.path repo}/projects/#{number}", opts
+        patch "projects/#{id}", opts
       end
 
       # Delete a project
       #
       # Requires authenticated client
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
+      # @param id [Integer] Project id
       # @return [Boolean] Result of deletion
-      # @see https://developer.github.com/v3/repos/projects/#delete-a-project
+      # @see https://developer.github.com/v3/projects/#delete-a-project 
       # @example
-      #   @client.delete_project("octokit/octokit.rb", 1)
-      def delete_project(repo, number, options = {})
+      #   @client.delete_project(123942)
+      def delete_project(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        boolean_from_response :delete, "#{Repository.path repo}/projects/#{number}", opts
+        boolean_from_response :delete, "projects/#{id}", opts
       end
 
-      # Get project columns
+      # List project columns
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
+      # @param id [Integer] Project id 
       # @return [Array<Sawyer::Resource>] List of project columns
-      # @see https://developer.github.com/v3/repos/projects/#list-columns
+      # @see https://developer.github.com/v3/projects/columns/#list-project-columns 
       # @example
       #   @client.project_columns("octokit/octokit.rb", 1)
-      def project_columns(repo, number, options = {})
+      def project_columns(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        paginate "#{Repository.path repo}/projects/#{number}/columns", opts
+        paginate "projects/#{id}/columns", opts
       end
 
       # Create a project column
       #
       # Requires authenticated client
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
+      # @param id [Integer] Project column id
       # @param name [String] New column name
       # @return [Sawyer::Resource] Newly created column
-      # @see https://developer.github.com/v3/repos/projects/#create-a-column
+      # @see https://developer.github.com/v3/projects/columns/#create-a-project-column 
       # @example
       #   @client.create_project_column("octokit/octokit.rb", 1, "To Dones")
-      def create_project_column(repo, number, name, options = {})
+      def create_project_column(id, name, options = {})
         opts = ensure_api_media_type(:projects, options)
         opts[:name] = name
-        post "#{Repository.path repo}/projects/#{number}/columns", opts
+        post "projects/#{id}/columns", opts
       end
 
       # Get a project column by ID
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
-      # @param id [Integer] Column id
+      # @param id [Integer] Project column id
       # @return [Sawyer::Resource] Project column
-      # @see https://developer.github.com/v3/repos/projects/#get-a-column
+      # @see https://developer.github.com/v3/projects/columns/#get-a-project-column 
       # @example
-      #   Octokit.project_column("octokit/octokit.rb", 1, 30)
-      def project_column(repo, number, id, options = {})
+      #   Octokit.project_column(123940, 30)
+      def project_column(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        get "#{Repository.path repo}/projects/#{number}/columns/#{id}", opts
+        get "projects/columns/#{id}", opts
       end
 
       # Update a project column
       #
       # Requires authenticated client
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
-      # @param id [Integer] Column ID
+      # @param id [Integer] Project column id
       # @param name [String] New column name
       # @return [Sawyer::Resource] Updated column
-      # @see https://developer.github.com/v3/repos/projects/#update-a-column
+      # @see https://developer.github.com/v3/projects/columns/#update-a-project-column 
       # @example
-      #   @client.update_project_column("octokit/octokit.rb", 1, 30294, "new column name")
-      def update_project_column(repo, number, id, name, options = {})
+      #   @client.update_project_column(30294, "new column name")
+      def update_project_column(id, name, options = {})
         opts = ensure_api_media_type(:projects, options)
         opts[:name] = name
-        patch "#{Repository.path repo}/projects/#{number}/columns/#{id}", opts
+        patch "projects/columns/#{id}", opts
       end
 
       # Delete a project column
       #
       # Requires authenticated client
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
       # @param id [Integer] Project column id
       # @return [Boolean] Result of deletion request, true when deleted
-      # @see https://developer.github.com/v3/repos/projects/#delete-a-column
+      # @see https://developer.github.com/v3/projects/columns/#delete-a-project-column 
       # @example
-      #   @client.delete_project_column("octokit/octokit.rb", 1, 30294)
-      def delete_project_column(repo, number, id, options = {})
+      #   @client.delete_project_column(30294)
+      def delete_project_column(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        boolean_from_response :delete, "#{Repository.path repo}/projects/#{number}/columns/#{id}", opts
+        boolean_from_response :delete, "projects/columns/#{id}", opts
       end
 
       # Move a project column
       #
       # Requires authenticated client
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
       # @param id [Integer] Project column id
       # @param position [String] New position for the column. Can be one of 
       #   <tt>first</tt>, <tt>last</tt>, or <tt>after:<column-id></tt>, where
       #   <tt><column-id></tt> is the id value of a column in the same project.
       # @return [Boolean] Move result, true if successful
-      # @see https://developer.github.com/v3/repos/projects/#move-a-column
+      # @see https://developer.github.com/v3/projects/columns/#move-a-project-column
       # @example
-      #   @client.move_project_column("octokit/octokit.rb", 1, 3049, "last")
-      def move_project_column(repo, number, id, position, options = {})
+      #   @client.move_project_column(3049, "last")
+      def move_project_column(id, position, options = {})
         opts = ensure_api_media_type(:projects, options)
-        boolean_from_response :post, "#{Repository.path repo}/projects/#{number}/columns/#{id}/moves", opts
+        opts[:position] = position
+        boolean_from_response :post, "projects/columns/#{id}/moves", opts
       end
 
-      # Get the list of cards for a project column
+      # List columns cards
       #
-      # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @param number [Integer] Project number
+      # Requires authenticated client
+      #
       # @param id [Integer] Project column id
-      # @return [Array<Sawyer::Resource>] List of cards in the column
-      # @see https://developer.github.com/v3/repos/projects/#list-projects-cards
+      # @return [Array<Sawyer::Resource>] Cards in the column
+      # @see https://developer.github.com/v3/projects/cards/#list-project-cards
       # @example
-      #   @client.projects_cards("octokit/octokit.rb", 1, 39484)
-      def projects_cards(repo, number, id, optons = {})
+      #   @client.column_cards(123847)
+      def column_cards(id, options = {})
         opts = ensure_api_media_type(:projects, options)
-        paginate "#{Repository.path repo}/projects/#{number}/columns/#{id}/cards", opts
+        paginate "projects/columns/#{id}/cards", opts
+      end
+
+      # Create project card
+      #
+      # Requires authenticated client
+      #
+      # @param id [Integer] Project column id
+      # @option options [String] :note Card contents for a note type
+      # @option options [Integer] :content_id Issue ID for the card contents
+      # @option options [String] :content_type Type of content to associate
+      #   with the card. <tt>Issue</tt> is presently the only avaiable value
+      # @note If :note is supplied, :content_id and :content_type must be
+      #   excluded. Similarly, if :content_id is supplied, :content_type must
+      #   be set and :note must not be included.
+      # @return [Sawyer::Resource] Newly created card
+      # @see https://developer.github.com/v3/projects/cards/#create-a-project-card
+      # @example Create a project card with a note
+      #   @client.create_project_card(123495, note: 'New note card')
+      # @example Create a project card for an repository issue
+      #   @client.create_project_card(123495, content_id: 1, content_type: 'Issue')
+      def create_project_card(id, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        post "projects/columns/#{id}/cards", opts
+      end
+
+      # Get a project card
+      #
+      # Requires authenticated client
+      #
+      # @param id [Integer] Project card id
+      # @return [Sawyer::Resource] Project card
+      # @see https://developer.github.com/v3/projects/cards/#get-a-project-card
+      # @example
+      #   @client.project_card(123495)
+      def project_card(id, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        get "projects/columns/cards/#{id}", opts
+      end
+
+      # Update a project card
+      #
+      # Requires authenticated client
+      #
+      # @param id [Integer] Project card id
+      # @option options [String] :note The card's note content. Only valid for
+      #   cards without another type of content, so this cannot be specified if
+      #   the card already has a content_id and content_type.
+      # @return [Sawyer::Resource] Updated project card
+      # @see https://developer.github.com/v3/projects/cards/#update-a-project-card
+      # @example
+      #   @client.update_project_card(12345, note: 'new note')
+      def update_project_card(id, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        patch "projects/columns/cards/#{id}", opts
+      end
+
+      # Move a project card
+      #
+      # Requires authenticated client
+      #
+      # @param id [Integer] Project card id
+      # @param position [String] Can be one of <tt>top</tt>, <tt>bottom</tt>,
+      #   or <tt>after:<card-id></tt>, where <card-id> is the id value of a
+      #   card in the same column, or in the new column specified by column_id.
+      # @option options [Integer] :column_id The column id to move the card to,
+      #   must be column in same project
+      # @return [Boolean] True if successful, false otherwise
+      # @see https://developer.github.com/v3/projects/cards/#move-a-project-card
+      # @example Move a card to the bottom of the same column
+      #   @client.move_project_card(123495, 'bottom')
+      # @example Move a card to the top of another column
+      #   @client.move_project_card(123495, 'top', column_id: 59402)
+      def move_project_card(id, position, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        opts[:position] = position
+        boolean_from_response :post, "projects/columns/cards/#{id}/moves", opts
+      end
+
+      # Delete a project card
+      # 
+      # Requires authenticated client
+      #
+      # @param id [Integer] Project card id
+      # @return [Boolean] True of deleted, false otherwise
+      # @see https://developer.github.com/v3/projects/cards/#delete-a-project-card
+      # @example
+      #   @client.delete_project_card(123495)
+      def delete_project_card(id, options = {})
+        opts = ensure_api_media_type(:projects, options)
+        boolean_from_response :delete, "projects/columns/cards/#{id}", opts
       end
 
     end # Projects
--- spec/octokit/client/projects_spec.rb
+++ spec/octokit/client/projects_spec.rb
@@ -18,28 +18,42 @@ describe Octokit::Client::Projects do
     end
   end # .create_project
 
+  describe ".org_projects", :vcr do
+    it "returns the projects for an organization" do
+      projects = oauth_client.org_projects(test_github_org)
+      expect(projects).to be_kind_of Array
+      assert_requested :get, github_url("/orgs/#{test_github_org}/projects")
+    end
+  end # .org_project
+
+  describe ".create_org_project", :vcr do
+    it "returns the new org project" do
+      project = oauth_client.create_org_project(test_github_org, "synergy", body: "do it")
+      expect(project.name).to eq "synergy"
+      expect(project.body).to eq "do it"
+      assert_requested :post, github_url("/orgs/#{test_github_org}/projects")
+    end
+  end # .create_org_project
+
   context "with repository" do
     before(:each) do
-      @repo = oauth_client.create_repository(test_github_repository)
+      delete_test_repo
     end
 
     after(:each) do
-      begin
-        oauth_client.delete_repository(@repo.full_name)
-      rescue Octokit::NotFound
-      end
+      delete_test_repo
     end
 
     context "with project" do
       before(:each) do
-        @project = oauth_client.create_project(@repo.full_name, "implement apis")
+        @project = oauth_client.create_project(@test_repo, "implement apis")
       end
 
       describe ".project", :vcr do
         it "returns a project" do
-          project = Octokit.project(@repo.full_name, @project.number)
+          project = oauth_client.project(@project.id)
           expect(project.name).not_to be_nil
-          assert_requested :get, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}")
+          assert_requested :get, github_url("/projects/#{@project.id}")
         end
       end # .project
 
@@ -47,87 +61,127 @@ describe Octokit::Client::Projects do
         it "updates the project name and body then returns the updated project" do
           name = "new name"
           body = "new body"
-          project = oauth_client.update_project(@repo.full_name, @project.number, {name: name, body: body})
+          project = oauth_client.update_project(@project.id, {name: name, body: body})
           expect(project.name).to eq name
           expect(project.body).to eq body
-          assert_requested :patch, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}")
+          assert_requested :patch, github_url("/projects/#{@project.id}")
         end
       end # .update_project
 
       describe ".delete_project", :vcr do
         it "returns the result of deleting a project" do
-          result = oauth_client.delete_project(@repo.full_name, @project.number)
+          result = oauth_client.delete_project(@project.id)
           expect(result).to eq true
-          assert_requested :delete, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}")
+          assert_requested :delete, github_url("/projects/#{@project.id}")
         end
       end # .delete_project
 
       describe ".project_columns", :vcr do
         it "returns the columns for a project" do
-          columns = Octokit.project_columns(@repo.full_name, @project.number)
+          columns = oauth_client.project_columns(@project.id)
           expect(columns).to be_kind_of Array
-          assert_requested :get, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}/columns")
+          assert_requested :get, github_url("/projects/#{@project.id}/columns")
         end
       end # .project_columns
 
       describe ".create_project_column", :vcr do
         it "returns the newly created project column" do
-          column = oauth_client.create_project_column(@repo.full_name, @project.number, "Todos")
+          column = oauth_client.create_project_column(@project.id, "Todos")
           expect(column.name).to eq "Todos"
-          assert_requested :post, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}/columns")
+          assert_requested :post, github_url("/projects/#{@project.id}/columns")
         end
       end # .create_project_column
 
       context "with project column" do
         before(:each) do
-          @column = oauth_client.create_project_column(@repo.full_name, @project.number, 'Todos')
+          @column = oauth_client.create_project_column(@project.id, "Todos #{Time.now.to_f}")
         end
 
         describe ".project_column", :vcr do
           it "returns a project column by id" do
-            skip "pending api"
-            column = oauth_client.project_column(@repo.full_name, @project.number, @column.id)
+            column = oauth_client.project_column(@column.id)
             expect(column.name).to be_kind_of String
-            assert_requested :get, github_url("/repos/#{@repo.full_name}/projects/#{@projects.number}/columns/#{@column.id}")
+            assert_requested :get, github_url("/projects/columns/#{@column.id}")
           end
         end # .project_column
 
         describe ".update_project_column", :vcr do
           it "updates the project column and returns the updated column" do
-            skip "pending api"
-            column = oauth_client.update_project_column(@repo.full_name, @project.number, @column.id, "new name")
+            column = oauth_client.update_project_column(@column.id, "new name")
             expect(column.name).to eq "new name"
-            assert_requested :patch, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}/columns/#{@column.id}")
+            assert_requested :patch, github_url("/projects/columns/#{@column.id}")
           end
         end # .update_project_column
 
-        describe ".delete_project_column", :vcr do
-          it "deletes the project column" do
-            skip "pending api"
-            result = oauth_client.delete_project_column(@repo.full_name, @project.number, @column.id)
-            expect(result).to eq true
-            assert_requested :delete, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}/columns/#{@column.id}")
-          end
-        end # .delete_project_column
-
         describe ".move_project_column", :vcr do
           it "moves the project column" do
-            skip "pending api"
-            result = oauth_client.move_project_column(@repo.full_name, @project.number, @column.id, "last")
-            expect(result).to eq true
-            assert_requested :post, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}/columns/#{@column.id}/moves")
+            result = oauth_client.move_project_column(@column.id, "last")
+            assert_requested :post, github_url("/projects/columns/#{@column.id}/moves")
           end
         end # .move_project_column
 
-        describe ".projects_cards", :vcr do
-          it "returns projects cards by column id" do
-            skip "pending api"
-            cards = oauth_client.projects_cards(@repo.full_name, @project.number, @column.id)
-            expect(cards).to be_kind_of Array
-            assert_requested :get, github_url("/repos/#{@repo.full_name}/projects/#{@project.number}/columns/#{@column.id}/cards")
+        describe ".delete_project_column", :vcr do
+          it "deletes the project column" do
+            result = oauth_client.delete_project_column(@column.id)
+            expect(result).to eq true
+            assert_requested :delete, github_url("/projects/columns/#{@column.id}")
           end
-        end # .projects_cards
+        end # .delete_project_column
 
+        describe ".column_cards", :vcr do
+          it "returns a list of the cards in a project column" do
+            cards = oauth_client.column_cards(@column.id)
+            expect(cards).to be_kind_of Array
+            assert_requested :get, github_url("/projects/columns/#{@column.id}/cards")
+          end
+        end # .column_cards
+
+        describe ".create_project_card", :vcr do
+          it "creates a new card with a note" do
+            card = oauth_client.create_project_card(@column.id, note: 'thing')
+            expect(card.note).to eq 'thing'
+            assert_requested :post, github_url("/projects/columns/#{@column.id}/cards")
+          end
+        end # .create_project_card
+
+        context "with project card" do
+          before(:each) do
+            @card = oauth_client.create_project_card(@column.id, note: 'octocard')
+          end
+
+          describe ".project_card", :vcr do
+            it "returns a project card by id" do
+              card = oauth_client.project_card(@card.id)
+              expect(card.note).to be_kind_of String
+              assert_requested :get, github_url("/projects/columns/cards/#{@card.id}")
+            end
+          end # .project_card
+
+          describe ".update_project_card", :vcr do
+            it "updates the project card" do
+              card = oauth_client.update_project_card(@card.id, note: 'new note')
+              expect(card.note).to eq 'new note'
+              assert_requested :patch, github_url("/projects/columns/cards/#{@card.id}")
+            end
+          end # .update_project_card
+
+          describe ".move_project_card", :vcr do
+            it "moves the project card" do
+              success = oauth_client.move_project_card(@card.id, 'bottom')
+              expect(success).to eq true
+              assert_requested :post, github_url("/projects/columns/cards/#{@card.id}/moves")
+            end
+          end # .move_project_card
+
+          describe ".delete_project_card", :vcr do
+            it "deletes the project card" do
+              success = oauth_client.delete_project_card(@card.id)
+              expect(success).to eq true
+              assert_requested :delete, github_url("/projects/columns/cards/#{@card.id}")
+            end
+          end # .delete_project_card
+
+        end # with project card
       end # with project column
     end # with project
   end # with repository
