9deaa7962b226ef2ac0d6d05465e51ab61bf7130
Steve Richert <steve.richert@gmail.com>
2019-01-17 11:07:59 -0500

    Add documentation to Octokit::Client::Checks#check_runs_for_ref
    [ci skip]

--- lib/octokit/client/checks.rb
+++ lib/octokit/client/checks.rb
@@ -46,6 +46,22 @@ module Octokit
         patch "#{Repository.path repo}/check-runs/#{id}", opts
       end
 
+      # List check runs for a specific ref
+      #
+      # @param repo [Integer, String, Hash, Repository] A GitHub repository
+      # @param ref [String] A SHA, branch name, or tag name
+      # @param options [Hash] A set of optional filters
+      # @option options [String] :check_name Returns check runs with the specified <tt>name</tt>
+      # @option options [String] :status Returns check runs with the specified <tt>status</tt>
+      # @option options [String] :filter Filters check runs by their <tt>completed_at</tt> timestamp
+      # @return [Sawyer::Resource] A hash representing a collection of check runs
+      # @see https://developer.github.com/v3/checks/runs/#list-check-runs-for-a-specific-ref
+      # @example List check runs for a specific ref
+      #   result = Octokit.check_runs_for_ref("octocat/Hello-World", "7638417db6d59f3c431d3e1f261cc637155684cd", status: "in_progress")
+      #   result.total_count # => 1
+      #   result.check_runs.count # => 1
+      #   result.check_runs[0].id #=> 51295429
+      #   result.check_runs[0].status #=> "in_progress"
       def check_runs_for_ref(repo, ref, options = {})
         opts = ensure_api_media_type(:checks, options)
 
