bc0ed0d70ad61bab2337057b473bddd1f1686b3a
Heather Harvey <hmharvey@github.com>
2020-01-07 17:24:27 -0800

    Fix/Add project collab tests

--- spec/octokit/client/projects_spec.rb
+++ spec/octokit/client/projects_spec.rb
@@ -36,6 +36,57 @@ describe Octokit::Client::Projects do
     end
   end # .create_org_project
 
+
+  context "with org project" do
+    before(:each) do
+      project = oauth_client.create_org_project(test_github_org, "org test project", accept: preview_header)
+      @project_id = project.id
+      @user = "hmharvey"
+    end
+
+    after(:each) do
+      project = oauth_client.delete_project(@project_id, accept: preview_header)
+    end
+
+    describe ".project_collaborators", :vcr do
+      it "returns a list of projects collaborators" do
+        collaborators = oauth_client.project_collaborators(@project_id, accept: preview_header)
+        expect(collaborators).to be_kind_of Array
+        assert_requested :get, github_url("/projects/#{@project_id}/collaborators")
+      end
+    end # .project_collaborators
+
+    describe ".add_project_collaborator", :vcr do
+      it "adds a project collaborator" do
+        result = oauth_client.add_project_collaborator(@project_id, @user, accept: preview_header)
+        expect(result).to eq true
+        assert_requested :put, github_url("/projects/#{@project_id}/collaborators/#{@user}")
+      end
+    end # .add_project_collaborator
+
+    context "with collaborator" do
+      before(:each) do
+        result = oauth_client.add_project_collaborator(@project_id, @user, accept: preview_header)
+      end
+
+      describe ".remove_project_collaborator", :vcr do
+        it "removes a project collaborator" do
+          result = oauth_client.remove_project_collaborator(@project_id, @user, accept: preview_header)
+          expect(result).to eq true
+          assert_requested :put, github_url("/projects/#{@project_id}/collaborators/#{@user}")
+        end
+      end # .remove_project_collaborator
+
+      describe ".user_permission_level", :vcr do
+        it "returns a user's permission level" do
+          result = oauth_client.user_permission_level(@project_id, @user, accept: preview_header)
+          expect(result.permission).to be_kind_of String
+          assert_requested :get, github_url("/projects/#{@project_id}/collaborators/#{@user}/permission")
+        end
+      end # .user_permission_level
+    end # with collaborator
+  end # with org project
+
   context "with repository" do
     context "with project" do
       before(:each) do
@@ -183,14 +234,6 @@ describe Octokit::Client::Projects do
           end # .delete_project_card
         end # with project card
       end # with project column
-
-      describe ".project_collaborators", :vcr do
-        it "returns a list of projects collaborators" do
-          # binding.pry
-          result = oauth_client.project_collaborators(@project.id, accept: preview_header)
-          assert_requested :get, github_url("/projects/#{@project.id}/collaborators")
-        end
-      end
     end # with project
   end # with repository
 
