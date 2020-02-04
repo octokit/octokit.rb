df350049b0c8ea5d2928d2f2ae0f20157e6cb1bb
Mark Tareshawty <mark@marktareshawty.com>
2020-02-04 11:24:09 -0500

    Actually we can send the authorization header, it won't matter.

--- lib/octokit/client/users.rb
+++ lib/octokit/client/users.rb
@@ -54,16 +54,7 @@ module Octokit
           }
         })
 
-        # In the event that someone passed in the client_id
-        # and client_secret as part of the Octokit::Client
-        # initialization, we need to clear them out.
-        #
-        # This is because the client_id and client_secret
-        # are now used as part of Basic Authentication.
-        client = self.dup
-        client.client_id = client.client_secret = nil
-
-        client.post "#{web_endpoint}login/oauth/access_token", options
+        post "#{web_endpoint}login/oauth/access_token", options
       end
 
       # Validate user username and password
