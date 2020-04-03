b4f2cb896a146a1c8d72c7ed0fe36c3d5506fb3a
hmharvey <hmharvey@github.com>
2020-04-03 13:04:41 -0700

    Generate clients

--- lib/octokit/client/notifications.rb
+++ lib/octokit/client/notifications.rb
@@ -6,32 +6,32 @@ module Octokit
     #
     # @see https://developer.github.com/v3/activity/notifications/
     module Notifications
-      # List your notifications
+      # List notifications for the authenticated user
       #
       # @option options [Boolean] :all If true, show notifications marked as read.
       # @option options [Boolean] :participating If true, only shows notifications in which the user is directly participating or mentioned.
       # @option options [String] :since Only show notifications updated after the given time. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ.
       # @option options [String] :before Only show notifications updated before the given time. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ.
       # @return [Array<Sawyer::Resource>] A list of notifications
-      # @see https://developer.github.com/v3/activity/notifications/#list-your-notifications
-      def notifications(options = {})
+      # @see https://developer.github.com/v3/activity/notifications/#list-notifications-for-the-authenticated-user
+      def user_notifications(options = {})
         paginate 'notifications', options
       end
 
-      # Mark as read
+      # Mark notifications as read
       #
-      # @option options [String] :last_read_at Describes the last point that notifications were checked. Anything updated since this time will not be updated. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ. Default: The current timestamp.
+      # @option options [String] :last_read_at Describes the last point that notifications were checked. Anything updated since this time will not be marked as read. If you omit this parameter, all notifications are marked as read. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ. Default: The current timestamp.
       # @return [Boolean] True on success, false otherwise
-      # @see https://developer.github.com/v3/activity/notifications/#mark-as-read
-      def mark_as_read(options = {})
+      # @see https://developer.github.com/v3/activity/notifications/#mark-notifications-as-read
+      def mark_notifications_as_read(options = {})
         boolean_from_response :put, 'notifications', options
       end
 
-      # View a single thread
+      # Get a thread
       #
       # @param thread_id [Integer] The ID of the thread
       # @return [Sawyer::Resource] A single thread
-      # @see https://developer.github.com/v3/activity/notifications/#view-a-single-thread
+      # @see https://developer.github.com/v3/activity/notifications/#get-a-thread
       def thread(thread_id, options = {})
         get "notifications/threads/#{thread_id}", options
       end
@@ -45,12 +45,12 @@ module Octokit
         boolean_from_response :patch, "notifications/threads/#{thread_id}", options
       end
 
-      # Get a thread subscription
+      # Get a thread subscription for the authenticated user
       #
       # @param thread_id [Integer] The ID of the thread
       # @return [Sawyer::Resource] A single subscription
-      # @see https://developer.github.com/v3/activity/notifications/#get-a-thread-subscription
-      def thread_subscription(thread_id, options = {})
+      # @see https://developer.github.com/v3/activity/notifications/#get-a-thread-subscription-for-the-authenticated-user
+      def user_thread_subscription(thread_id, options = {})
         get "notifications/threads/#{thread_id}/subscription", options
       end
 
@@ -73,7 +73,7 @@ module Octokit
         boolean_from_response :delete, "notifications/threads/#{thread_id}/subscription", options
       end
 
-      # List your notifications in a repository
+      # List repository notifications for the authenticated user
       #
       # @param repo [Integer, String, Repository, Hash] A GitHub repository
       # @option options [Boolean] :all If true, show notifications marked as read.
@@ -81,18 +81,18 @@ module Octokit
       # @option options [String] :since Only show notifications updated after the given time. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ.
       # @option options [String] :before Only show notifications updated before the given time. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ.
       # @return [Array<Sawyer::Resource>] A list of notifications
-      # @see https://developer.github.com/v3/activity/notifications/#list-your-notifications-in-a-repository
-      def repository_notifications(repo, options = {})
+      # @see https://developer.github.com/v3/activity/notifications/#list-repository-notifications-for-the-authenticated-user
+      def user_repo_notifications(repo, options = {})
         paginate "#{Repository.path repo}/notifications", options
       end
 
-      # Mark notifications as read in a repository
+      # Mark repository notifications as read
       #
       # @param repo [Integer, String, Repository, Hash] A GitHub repository
-      # @option options [String] :last_read_at Describes the last point that notifications were checked. Anything updated since this time will not be updated. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ. Default: The current timestamp.
+      # @option options [String] :last_read_at Describes the last point that notifications were checked. Anything updated since this time will not be marked as read. If you omit this parameter, all notifications are marked as read. This is a timestamp in ISO 8601 (https://en.wikipedia.org/wiki/ISO_8601) format: YYYY-MM-DDTHH:MM:SSZ. Default: The current timestamp.
       # @return [Boolean] True on success, false otherwise
-      # @see https://developer.github.com/v3/activity/notifications/#mark-notifications-as-read-in-a-repository
-      def mark_repository_notifications_as_read(repo, options = {})
+      # @see https://developer.github.com/v3/activity/notifications/#mark-repository-notifications-as-read
+      def mark_repo_notifications_as_read(repo, options = {})
         boolean_from_response :put, "#{Repository.path repo}/notifications", options
       end
     end
