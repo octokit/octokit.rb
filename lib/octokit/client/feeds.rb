85683ba5555624c2d564c4272ede5aa9cb456ac2
hmharvey <hmharvey@github.com>
2020-04-10 12:48:15 -0700

    Generate feeds client

--- lib/octokit/client/feeds.rb
+++ lib/octokit/client/feeds.rb
@@ -1,33 +1,19 @@
+# frozen_string_literal: true
+
 module Octokit
   class Client
-
     # Methods for the Feeds API
     #
     # @see https://developer.github.com/v3/activity/feeds/
     module Feeds
-
-      # List Feeds
+      # Get feeds
       #
-      # The feeds returned depend on authentication, see the GitHub API docs
-      # for more information.
       #
-      # @return [Array<Sawyer::Resource>] list of feeds
-      # @see https://developer.github.com/v3/activity/feeds/#list-feeds
-      def feeds
-        get "feeds"
+      # @return [Sawyer::Resource] A single feeds
+      # @see https://developer.github.com/v3/activity/feeds/#get-feeds
+      def feeds(options = {})
+        get 'feeds', options
       end
-
-      # Get a Feed by name
-      #
-      # @param name [Symbol, String] Name of feed to retrieve.
-      # @return [Feed] Parsed feed in the format returned by the configured
-      #   parser.
-      def feed(name, options = {})
-        if rel = feeds._links[name]
-          get rel.href, :accept => rel.type, :options => options
-        end
-      end
-
     end
   end
 end
