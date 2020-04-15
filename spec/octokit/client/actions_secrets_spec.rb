5204c1d156901fde48e20cf0de6333155b144e06
Juha Ylitalo <juha.ylitalo@reaktor.com>
2020-04-15 19:34:46 +0300

    [actions_secrets] Split encyrption box creation into separate method

--- spec/octokit/client/actions_secrets_spec.rb
+++ spec/octokit/client/actions_secrets_spec.rb
@@ -2,6 +2,14 @@ require 'helper'
 require 'rbnacl'
 require 'base64'
 
+def create_box(public_key)
+  b64_key = RbNaCl::PublicKey.new(Base64.decode64(public_key[:key]))
+  {
+    key_id: public_key[:key_id],
+    box: RbNaCl::Boxes::Sealed.from_public_key(b64_key)
+  }
+end
+
 describe Octokit::Client::ActionsSecrets do
 
   before do
@@ -17,28 +25,27 @@ describe Octokit::Client::ActionsSecrets do
 
     after(:each) do
       begin
-        @client.delete_repository(@repo.full_name)
+        @client.delete_repository(@repo.full_name) unless @repo.nil?
       rescue Octokit::NotFound
       end
     end
 
     describe ".get_public_key", :vcr do
       it "get repo specific public key for secrets encryption" do
-        public_key = @client.get_public_key(@repo.id)
-        b64_key = RbNaCl::PublicKey.new(Base64.decode64(public_key[:key]))
-        expect(public_key[:key_id]).not_to be_empty
+        box = create_box(@client.get_public_key(@repo.id))
+        expect(box[:key_id]).not_to be_empty
       end
     end # .get_public_key
   end
 
-  context "with repo without secrets" do
+  context "with a repo without secrets" do
     before(:each) do
       @repo = @client.create_repository("an-repo")
     end
 
     after(:each) do
       begin
-        @client.delete_repository(@repo.full_name)
+        @client.delete_repository(@repo.full_name) unless @repo.nil?
       rescue Octokit::NotFound
       end
     end
@@ -53,13 +60,11 @@ describe Octokit::Client::ActionsSecrets do
 
     describe ".create_or_update_secret", :vcr do
       it "creating secret returns 201" do
-        public_key = @client.get_public_key(@repo.id)
-        @key_id = public_key[:key_id]
-        @box = RbNaCl::PublicKey.new(Base64.decode64(public_key[:key]))
-        encrypt = @box.encrypt(@secret[:value])
+        box = create_box(@client.get_public_key(@repo.id))
+        encrypted = box[:box].encrypt(@secret[:value])
         resp = @client.create_or_update_secret(
           @repo.id, @secret[:name],
-          key_id: @key_id, encrypted_value: Base64.strict_encode64(encrypted)
+          key_id: box[:key_id], encrypted_value: Base64.strict_encode64(encrypted)
         )
         expect(resp.status).to eq(201)
       end
@@ -67,22 +72,20 @@ describe Octokit::Client::ActionsSecrets do
   end
 
 
-  context "with repository with a secret" do
+  context "with a repository with a secret" do
     before(:each) do
       @repo = @client.create_repository("an-repo")
-      public_key = @client.get_public_key(@repo.id)
-      @key_id = public_key[:key_id]
-      @box = RbNaCl::PublicKey.new(Base64.decode64(public_key[:key]))
-      encrypt = @box.encrypt(@secret[:value])
+      @box = create_box(@client.get_public_key(@repo.id))
+      encrypted = @box[:box].encrypt(@secret[:value])
       @client.create_or_update_secret(
         @repo.id, @secret[:name],
-        key_id: @key_id, encrypted_value: Base64.strict_encode64(encrypted)
+        key_id: @box[:key_id], encrypted_value: Base64.strict_encode64(encrypted)
       )
     end
 
     after(:each) do
       begin
-        @client.delete_repository(@repo.full_name)
+        @client.delete_repository(@repo.full_name) unless @repo.nil?
       rescue Octokit::NotFound
       end
     end
@@ -104,13 +107,11 @@ describe Octokit::Client::ActionsSecrets do
 
     describe ".create_or_update_secret", :vcr do
       it "updating existing secret returns 204" do
-        public_key = @client.get_public_key(@repo.id)
-        @key_id = public_key[:key_id]
-        @box = RbNaCl::PublicKey.new(Base64.decode64(public_key[:key]))
-        encrypt = @box.encrypt("new value")
+        box = create_box(@client.get_public_key(@repo.id))
+        encrypted = box[:box].encrypt("new value")
         resp = @client.create_or_update_secret(
           @repo.id, @secret[:name],
-          key_id: @key_id, encrypted_value: Base64.strict_encode64(encrypted)
+          key_id: box[:key_id], encrypted_value: Base64.strict_encode64(encrypted)
         )
         expect(resp.status).to eq(204)
       end
