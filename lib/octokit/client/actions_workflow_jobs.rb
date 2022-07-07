77459347b82eccbd7d80ca3845d851bb252c297f
Mathieu Rul <mathroule@gmail.com>
2022-07-07 08:40:58 +0200

    Add `workflow_run_attempt_jobs`

--- lib/octokit/client/actions_workflow_jobs.rb
+++ lib/octokit/client/actions_workflow_jobs.rb
@@ -6,6 +6,19 @@ module Octokit
     #
     # @see https://docs.github.com/rest/actions/workflow-jobs
     module ActionsWorkflowJobs
+      # List jobs for a workflow run attempt
+      #
+      # @param repo [Integer, String, Repository, Hash] A GitHub repository
+      # @param run_id [Integer, String] Id of the workflow run
+      # @param attempt_number [Integer, String] Attempt number of the workflow run
+      #
+      # @return [Sawyer::Resource] Jobs information
+      # @see https://docs.github.com/rest/actions/workflow-jobs#list-jobs-for-a-workflow-run-attempt
+      def workflow_run_attempt_jobs(repo, run_id, attempt_number, options = {})
+        paginate "#{Repository.path repo}/actions/runs/#{run_id}/attempts/#{attempt_number}/jobs", options
+      end
+      alias list_workflow_run_attempt_jobs workflow_run_attempt_jobs
+
       # List jobs for a workflow run
       #
       # @param repo [Integer, String, Repository, Hash] A GitHub repository
--- spec/octokit/client/actions_workflow_jobs_spec.rb
+++ spec/octokit/client/actions_workflow_jobs_spec.rb
@@ -7,10 +7,21 @@ describe Octokit::Client::ActionsWorkflowJobs, :vcr do
     Octokit.reset!
     @client = oauth_client
     @run_id = 96_922_843
+    @attempt_number = 1
   end
 
   after do
     Octokit.reset!
+  end
+
+  describe '.workflow_run_attempt_jobs' do
+    it 'returns jobs for a workflow run attempt' do
+      request = stub_get("repos/#{@test_repo}/actions/runs/#{@run_id}/attempts/#{@attempt_number}/jobs")
+
+      @client.workflow_run_attempt_jobs(@test_repo, @run_id, @attempt_number)
+
+      assert_requested request
+    end
   end
 
   describe '.workflow_run_jobs' do
