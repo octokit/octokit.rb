8c9a38c85061ba169276a5a3644b2e7cdd4e5fdf
Keegan Campbell <me@kfcampbell.com>
2022-10-18 12:36:24 -0700

    Remove create_authorization in favor of hardcoded test tokens

--- lib/octokit/client/tokens.rb
+++ lib/octokit/client/tokens.rb
@@ -6,54 +6,6 @@ module Octokit
     #
     # @see https://developer.github.com/v3/oauth_authorizations/#oauth-authorizations-api
     module Tokens
-      # Create an authorization for the authenticated user.
-      #
-      # You can create your own tokens, and only through
-      # Basic Authentication.
-      #
-      # @param options [Hash] A customizable set of options.
-      # @option options [Array] :scopes A list of scopes that this authorization is in.
-      # @option options [String] :note A note to remind you what the OAuth token is for.
-      # @option options [String] :note_url A URL to remind you what app the OAuth token is for.
-      # @option options [Boolean] :idempotent If true, will return an existing authorization if one has already been created.
-      # @option options [String] :client_id  Client Id we received when our application was registered with GitHub.
-      # @option options [String] :client_secret  Client Secret we received when our application was registered with GitHub.
-      #
-      # @return [Sawyer::Resource] A single authorization for the authenticated user
-      # @see https://developer.github.com/v3/oauth/#scopes Available scopes
-      # @see https://developer.github.com/v3/oauth_authorizations/#create-a-new-authorization
-      # @see https://developer.github.com/v3/oauth_authorizations/#get-or-create-an-authorization-for-a-specific-app
-      # @example Create a new authorization for user ctshryock's project Zoidberg
-      #  client = Octokit::Client.new(:login => 'ctshryock', :password => 'secret')
-      #  client.create_authorization({:scopes => ["public_repo", "gist"], :note => "Why not Zoidberg?", :note_url=> "https://en.wikipedia.org/wiki/Zoidberg"})
-      # @example Create a new OR return an existing authorization to be used by a specific client for user ctshryock's project Zoidberg
-      #  client = Octokit::Client.new(:login => 'ctshryock', :password => 'secret')
-      #  client.create_authorization({:idempotent => true, :client_id => 'xxxx', :client_secret => 'yyyy', :scopes => ["user"]})
-      def create_authorization(options = {})
-        # Technically we can omit scopes as GitHub has a default, however the
-        # API will reject us if we send a POST request with an empty body.
-        options = options.dup
-        if options.delete :idempotent
-          client_id, client_secret = fetch_client_id_and_secret(options)
-          unless client_id && client_secret
-            raise ArgumentError, 'Client ID and Secret required for idempotent authorizations'
-          end
-
-          # Remove the client_id from the body otherwise
-          # this will result in a 422.
-          options.delete(:client_id)
-
-          if (fingerprint = options.delete(:fingerprint))
-            put "authorizations/clients/#{client_id}/#{fingerprint}", options.merge(client_secret: client_secret)
-          else
-            put "authorizations/clients/#{client_id}", options.merge(client_secret: client_secret)
-          end
-
-        else
-          post 'authorizations', options
-        end
-      end
-
       # Check scopes for a token
       #
       # @param token [String] GitHub OAuth token
--- spec/octokit/client/tokens_spec.rb
+++ spec/octokit/client/tokens_spec.rb
@@ -21,111 +21,22 @@ describe Octokit::Client::Tokens do
     "Note #{SecureRandom.hex(20)}"
   end
 
-  describe '.create_authorization', :vcr do
-    context 'without :idempotent => true' do
-      it 'creates an API authorization' do
-        authorization = @client.create_authorization(note: note)
-        expect(authorization.app.name).not_to be_nil
-        expect(WebMock).to have_requested(:post, github_url('/authorizations')).with(
-          basic_auth: [
-            test_github_login,
-            test_github_password
-          ]
-        )
-      end
-
-      it 'creates a new API authorization each time' do
-        first_authorization = @client.create_authorization(note: note)
-        second_authorization = @client.create_authorization(note: note)
-        expect(first_authorization.id).not_to eq(second_authorization.id)
-      end
-
-      it 'creates a new authorization with options' do
-        info = {
-          note: note,
-          scope: ['gist']
-        }
-        authorization = @client.create_authorization info
-        expect(authorization.scopes).to be_kind_of Array
-        expect(WebMock).to have_requested(:post, github_url('/authorizations')).with(
-          basic_auth: [
-            test_github_login,
-            test_github_password
-          ]
-        )
-      end
-    end
-
-    context 'with :idempotent => true' do
-      it 'creates a new authorization with options' do
-        authorization = @client.create_authorization(
-          idempotent: true,
-          client_id: test_github_client_id,
-          client_secret: test_github_client_secret,
-          scopes: %w[gist]
-        )
-
-        expect(authorization.scopes).to be_kind_of Array
-        expect(WebMock).to have_requested(:put, github_url("/authorizations/clients/#{test_github_client_id}")).with(
-          basic_auth: [
-            test_github_login,
-            test_github_password
-          ]
-        )
-      end
-
-      it 'creates a new authorization with fingerprint' do
-        path = "/authorizations/clients/#{test_github_client_id}/jklmnop12345678"
-
-        @client.create_authorization(
-          idempotent: true,
-          client_id: test_github_client_id,
-          client_secret: test_github_client_secret,
-          scopes: %w[gist],
-          fingerprint: 'jklmnop12345678'
-        )
-
-        expect(WebMock).to have_requested(:put, github_url(path)).with(
-          basic_auth: [
-            test_github_login,
-            test_github_password
-          ]
-        )
-      end
-
-      it 'returns an existing API authorization if one already exists' do
-        options = {
-          idempotent: true,
-          client_id: test_github_client_id,
-          client_secret: test_github_client_secret
-        }
-
-        first_authorization  = @client.create_authorization(options)
-        second_authorization = @client.create_authorization(options)
-
-        expect(first_authorization.id).to eql second_authorization.id
-      end
-    end
-  end # .create_authorization
-
   describe '.scopes', :vcr do
     it 'checks the scopes on the current token' do
-      authorization = @client.create_authorization(note: note)
-      use_vcr_placeholder_for(authorization.token, 'SCOPE_AUTHORIZATION_TOKEN')
+      use_vcr_placeholder_for('dummytoken123456', 'SCOPE_AUTHORIZATION_TOKEN')
 
-      token_client = Octokit::Client.new(access_token: authorization.token)
+      token_client = Octokit::Client.new(access_token: 'dummytoken123456')
 
       expect(token_client.scopes).to be_kind_of Array
       assert_requested :get, github_url('/user')
     end
 
     it 'checks the scopes on a one-off token' do
-      authorization = @client.create_authorization(note: note)
-      use_vcr_placeholder_for(authorization.token, 'ONE_OFF_SCOPE_AUTHORIZATION_TOKEN')
+      use_vcr_placeholder_for('dummytoken123456', 'ONE_OFF_SCOPE_AUTHORIZATION_TOKEN')
 
       Octokit.reset!
 
-      expect(Octokit.scopes(authorization.token)).to be_kind_of Array
+      expect(Octokit.scopes('dummytoken123456')).to be_kind_of Array
       assert_requested :get, github_url('/user')
     end
   end # .scopes
