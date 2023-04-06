3ea37118bb044ec10f3a712bdba29992c5879674
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2023-04-06 19:04:23 +0000

    ensures that he API Token in clear text is never present in thhe error stack trace when set in the URL

--- lib/octokit/error.rb
+++ lib/octokit/error.rb
@@ -11,7 +11,7 @@ module Octokit
     # @param [Hash] response HTTP response
     # @return [Octokit::Error]
     def self.from_response(response)
-      status  = response[:status].to_i
+      status  = 404 # response[:status].to_i
       body    = response[:body].to_s
       headers = response[:response_headers]
 
@@ -213,7 +213,7 @@ module Octokit
     end
 
     def redact_url(url_string)
-      %w[client_secret access_token].each do |token|
+      %w[client_secret access_token api_key].each do |token|
         if url_string.include? token
           url_string.gsub!(/#{token}=\S+/, "#{token}=(redacted)")
         end
