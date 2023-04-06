4da20d1f41f7047c18c256794c9a0758fa3e379b
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2023-04-06 19:31:51 +0000

    properly names the describe

--- spec/octokit/error_spec.rb
+++ spec/octokit/error_spec.rb
@@ -6,7 +6,7 @@ require 'octokit/error'
 # given the nature of this method and the fact that it is meant to keep secrets out of logs.
 # In this case, it is worth it and will help us know if something changes in this area.
 describe Octokit::Error do
-  describe '.build_error_message' do
+  describe '.redact_url' do
     it 'redacts api_key' do
       url = 'http://127.0.0.1:8989/setup/api/settings?api_key=++++THIS+IS+A+SECRET++++&settings=certificate%22%2C%22key%22%3A%22++++THIS+IS+A+SECRET+AS+WELL++++%22%7D%7D'.dup
       error = Octokit::Error.new
