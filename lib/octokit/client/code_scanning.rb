c2f2f77c2ca6482862947871f19b463c07a9a7b0
Mathieu Rul <mathroule@gmail.com>
2023-06-12 22:59:45 +0200

    Add code scanning

---
+++ lib/octokit/client/code_scanning.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Octokit
+  class Client
+    # Methods for the code scanning alerts API
+    #
+    # @see https://docs.github.com/rest/code-scanning
+    module CodeScanning
+    end
+  end
+end
---
+++ spec/octokit/client/code_scanning_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+describe Octokit::Client::CodeScanning do
+  before do
+    @client = oauth_client
+  end
+end
