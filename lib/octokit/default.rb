2efe1b81822801516ec42f435a8d9d217c35aa2c
Akihiko Odaki <akihiko.odaki@daynix.com>
2024-10-19 21:11:45 +0900

    Duplicate the default middleware stack
    Duplicate the default middleware stack so that it won't be modified.

--- lib/octokit/default.rb
+++ lib/octokit/default.rb
@@ -139,7 +139,7 @@ module Octokit
       # from {MIDDLEWARE}
       # @return [Faraday::RackBuilder or Faraday::Builder]
       def middleware
-        MIDDLEWARE
+        MIDDLEWARE.dup
       end
 
       # Default GitHub password for Basic Auth from ENV
